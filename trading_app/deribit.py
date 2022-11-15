a5a18f7321e7d5e06551cced50645a895c6d5ed1
venoajie <59521823+venoajie@users.noreply.github.com>
2022-11-15 15:47:23 +0700

    update

--- trading_app/deribit.py
+++ trading_app/deribit.py
@@ -27,7 +27,7 @@ import orjson
 from utils import save_open_files
 
 from os.path import join, dirname
-from configuration import load_dotenv
+from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
