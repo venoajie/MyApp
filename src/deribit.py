c4c68558f2847769b0c5451aeadab2ffffa59f6c
venoajie <59521823+venoajie@users.noreply.github.com>
2022-11-17 07:54:50 +0700

    Update deribit.py

--- src/deribit.py
+++ src/deribit.py
@@ -188,8 +188,6 @@ class main:
                         
                         bids = data_orders['bids']
                         asks = data_orders['asks']
-                        best_bid_prc = bids[0][0]
-                        best_ask_prc = asks[0][0]
                         #save_open_files.save_file('order_books',data_orders)
                         
                         if message_channel == 'user.orders.ETH-PERPETUAL.raw':
@@ -197,9 +195,18 @@ class main:
                             log.debug(data_orders)
                         
                             if balance_eth not in none_data:
+                                        
                                 save_open_files.save_file_to_pickle('portfolio-eth.pkl', balance_eth)
                         # 
-                            
+                            if balance_eth  in none_data:
+                                        
+                                best_bid_prc = bids[0][0]
+                                best_ask_prc = asks[0][0]
+                                log.critical(best_bid_prc)
+                                log.critical(best_ask_prc)
+                                balance = save_open_files.open_file_pickle('portfolio-eth.pkl')
+                                log.warning(balance)
+                                    
                             data_portfolio: list = message['params']['data']
                             log.critical(data_portfolio)
                             balance_eth: list = data_portfolio ['balance']
