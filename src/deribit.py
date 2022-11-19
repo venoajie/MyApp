65cbd1fcca031600c4e07b11e03d4073b6e67517
venoajie <59521823+venoajie@users.noreply.github.com>
2022-11-20 01:32:16 +0700

    Update deribit.py

--- src/deribit.py
+++ src/deribit.py
@@ -303,11 +303,11 @@ class main:
                                         
                                     log.error(f'{instrument=}')
                                 
-                                    instrument_data = [o for o in instruments if o['instrument_name'] == instrument]   [0] 
-                                    open_orders_instrument = [] if open_orders == [] else [o for o in open_orders if o['instrument_name'] == instrument]  
+                                    instrument_data:dict = [o for o in instruments if o['instrument_name'] == instrument]   [0] 
+                                    open_orders_instrument:list = [] if open_orders == [] else [o for o in open_orders if o['instrument_name'] == instrument]  
                                     log.info(f'{open_orders_instrument=}')
                                     log.error(f'{instrument_data=}')
-                                    tick_size = instrument_data ['tick_size']
+                                    tick_size:float = instrument_data ['tick_size']
                                     min_trade_amount = instrument_data ['min_trade_amount']
                                     contract_size = instrument_data ['contract_size']
                                     expiration_timestamp = instrument_data ['expiration_timestamp']
