f7a4fffb7471ae24028da4d121938af172c9c6c2
venoajie <59521823+venoajie@users.noreply.github.com>
2022-11-19 19:09:42 +0700

    Update deribit2.py

--- src/deribit2.py
+++ src/deribit2.py
@@ -122,6 +122,12 @@ class main:
             self.loop.create_task(
             self.ws_operation(
                 operation='subscribe',
+                ws_channel='user.orders.future.ETH.raw'
+                )
+            )
+            self.loop.create_task(
+            self.ws_operation(
+                operation='subscribe',
                 ws_channel='user.orders.ETH-PERPETUAL.raw'
                 )
             )
