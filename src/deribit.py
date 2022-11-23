38f677aaa098fc28180ad82ac3ecf1fbd92f194e
venoajie <59521823+venoajie@users.noreply.github.com>
2022-11-23 16:51:43 +0700

    Update deribit.py

--- src/deribit.py
+++ src/deribit.py
@@ -616,6 +616,7 @@ def main_ ():
     client_id: str = parse_dotenv() ['client_id']
     client_secret: str = parse_dotenv() ['client_secret']
     ws_connection_url: str = 'wss://test.deribit.com/ws/api/v2'
+    # ws_connection_url: str = 'wss://www.deribit.com/ws/api/v2'
         
     main(
         ws_connection_url=ws_connection_url,
@@ -625,18 +626,4 @@ def main_ ():
 
 if __name__ == "__main__":
 
-    # DBT LIVE WebSocket Connection URL
-    # ws_connection_url: str = 'wss://www.deribit.com/ws/api/v2'
-    # DBT TEST WebSocket Connection URL
-    ws_connection_url: str = 'wss://test.deribit.com/ws/api/v2'
-    
-    # DBT Client ID
-    client_id: str = parse_dotenv() ['client_id']
-    # DBT Client Secret
-    client_secret: str = parse_dotenv() ['client_secret']
     main_()
-    #main(
-    #    ws_connection_url=ws_connection_url,
-    #    client_id=client_id,
-    #    client_secret= client_secret
-    #    )
