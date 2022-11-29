04cfe4366775c84d6b7c35c58872bc4970a8e276
venoajie <venoajie@googlemail.com>
2022-11-29 12:15:17 +0000

    update

--- src/deribit.py
+++ src/deribit.py
@@ -19,7 +19,7 @@ from loguru import logger as log
 from dataclassy import dataclass
 from dotenv import dotenv_values
 from dotenv import load_dotenv
-# user defined formula
+# user defined formula vfdgvfdsbv ffdsss
 from utils import pickling, formula
 from configuration import id_numbering
 import deribit_get
