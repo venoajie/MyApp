3e60663cf46716e874704480521e346ed3cc03bc
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-02 18:29:38 +0700

    update path downloader

--- src/downloader-data.py
+++ src/downloader-data.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# built ins
+from pathlib import Path
+from datetime import datetime
+
 # installed
 from loguru import logger as log
 from rocketry import Rocketry
-from rocketry.conds import scheduler_running, after_success, after_success,daily, every, minutely, hourly
-from utils import pickling
+from rocketry.conds import  every
+
 # user defined formula
-from utils import formula, time_modification
+from utils import formula, pickling
 from market_data import get_market_data
-from datetime import datetime
 
 symbol = 'ETH-PERPETUAL'
 currency = 'ETH'
@@ -19,21 +22,7 @@ app = Rocketry(config={'task_execution': 'async',
                        'restarting': 'relaunch',
                        'cycle_sleep': 1})
 
-#@app.task(every("30 seconds"))
-def check_and_save_every_1_minutes ():
-
-    try:
-        
-        time_frame = 1
-        file_name =(f'{symbol.lower()}-ohlc-{time_frame}m.pkl')
-        ohlc = market_data. ohlc (time_frame, 500)
-        
-        pickling.replace_data(file_name, ohlc)
-        
-    except Exception as error:
-        import traceback
-        log.error(f"{error}")
-        log.error(traceback.format_exc())
+root = Path(".")
 
 @app.task(every("300 seconds"))
 def check_and_save_every_5_minutes ():
@@ -42,15 +31,18 @@ def check_and_save_every_5_minutes ():
                
         open_interest_historical = market_data. open_interest_historical ()
         file_name =(f'{currency.lower()}-openInterestHistorical.pkl')
-        pickling.replace_data(file_name, open_interest_historical)
+        my_path = root / "market_data" / file_name
+        pickling.replace_data(my_path, open_interest_historical)
         
         open_interest_symbol = market_data. open_interest_symbol ()
         file_name =(f'{currency.lower()}-openInterestSymbol.pkl')
-        pickling.replace_data(file_name, open_interest_symbol)
+        my_path = root / "market_data" / file_name
+        pickling.replace_data(my_path, open_interest_symbol)
         
         open_interest_aggregated_ohlc = market_data. open_interest_aggregated_ohlc ()
         file_name =(f'{currency.lower()}-openInterestAggregated.pkl')
-        pickling.replace_data(file_name, open_interest_aggregated_ohlc)
+        my_path = root / "market_data" / file_name
+        pickling.replace_data(my_path, open_interest_aggregated_ohlc)
         
     except Exception as error:
         import traceback
