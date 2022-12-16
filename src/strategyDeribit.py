79706f5ee2e7b65afab777fe1767472ee3c4511d
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-16 17:07:41 +0700

    Update strategyDeribit.py

--- src/strategyDeribit.py
+++ src/strategyDeribit.py
@@ -325,12 +325,16 @@ class strategyDeribit:
                                         open_orders_byBot: list = open_orders.my_orders_api()
                                         open_orders_Hedging = ([o  for o in open_orders_byBot if o['label'] == "hedging spot"])
                                         open_orders_HedgingSum = sum([o['amount'] for o in open_orders_Hedging ])
-                                        if open_orders_HedgingSum > hedging_size:
-                                            open_orders_Hedging_lastUpdateTStamps = ([o['last_update_timestamp'] for o in open_orders_Hedging ])
+                                        if  spot_hedging.is_over_hedged (open_orders_byBot, spot_hedged ['hedging_size']):
+                                            open_orders_Hedging_lastUpdateTStamps: list = open_orders.my_orders_api_last_update_timestamps()
                                             open_orders_Hedging_lastUpdateTStamp_min = min(open_orders_Hedging_lastUpdateTStamps)
                                             open_orders_Hedging_lastUpdateTStamp_minId = ([o['order_id'] for o in open_orders_byBot if o['last_update_timestamp'] == open_orders_Hedging_lastUpdateTStamp_min])[0]
-                                            await deribit_get.get_cancel_order_byOrderId(client_id, client_secret, endpointCancel, open_orders_Hedging_lastUpdateTStamp_minId)
                                             
+                                            await deribit_rest.get_cancel_order_byOrderId (self.connection_url, 
+                                                                                          client_id, 
+                                                                                          client_secret, 
+                                                                                          open_orders_Hedging_lastUpdateTStamp_minId
+                                                                                          )
             else:
                 log.info('WebSocket connection has broken.')
                 formula.log_error('WebSocket connection has broken','downloader-marketDeribit', 'error', 1)
