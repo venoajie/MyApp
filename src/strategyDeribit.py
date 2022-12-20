da3e8834d72fa057d1855040e67535f84ee3c682
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-20 11:16:31 +0700

    Update strategyDeribit.py

--- src/strategyDeribit.py
+++ src/strategyDeribit.py
@@ -269,7 +269,7 @@ class strategyDeribit:
                                 my_path_ordBook = system_tools.provide_path_for_file (file_name_ordBook, "market_data", "deribit")
                                 
                                 ordBook = pickling.read_data(my_path_ordBook)
-                                if ordBook ! =[] :
+                                if ordBook !=[] :
                                     max_time_stamp_ordBook = max ([o['timestamp'] for o in ordBook ])
                                     most_current_ordBook = [o for o in ordBook if o['timestamp'] == max_time_stamp_ordBook ]
 
