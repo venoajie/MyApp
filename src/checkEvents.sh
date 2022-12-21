961981b02ddd849ed0680c97d65d0f9ee4c8e0cb
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-21 13:58:43 +0700

    Update checkEvents.sh

--- src/checkEvents.sh
+++ src/checkEvents.sh
@@ -1,2 +1,3 @@
-while inotifywait -r -e modify,create,delete,move src/market_data/deribit &; do  ls -lh
+#!/bin/bash
+while inotifywait -r -e modify,create,delete,move src/market_data/deribit; do  ls -lh
 done
