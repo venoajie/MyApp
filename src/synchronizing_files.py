5e35f1217c82b40d3d89fc7eb1a329f952ad8806
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-28 13:19:17 +0700

    Update synchronizing_files.py

--- src/synchronizing_files.py
+++ src/synchronizing_files.py
@@ -369,7 +369,7 @@ class SynchronizingFiles ():
                                                 )
                         await self.cancel_redundant_orders_in_same_labels (currency, 'hedging spot-open')
                         
-                    if spot_was_unhedged == False and remain_unhedged >= 0 and len_open_orders_open_byAPI == []:
+                    if spot_was_unhedged == False and remain_unhedged <= 0 and len_open_orders_open_byAPI == []:
                         threshold = .025/100
                         label = f'hedging spot-closed'
                         await self.price_averaging (my_trades_open, threshold, currency, index_price, check_spot_hedging ['hedging_size'], label, best_bid_prc, best_ask_prc)
