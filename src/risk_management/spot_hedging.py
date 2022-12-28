292e5fd28fb49d281401ac8b3a9adf168e6bdcbf
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-28 11:21:42 +0700

    Update spot_hedging.py

--- src/risk_management/spot_hedging.py
+++ src/risk_management/spot_hedging.py
@@ -105,6 +105,7 @@ class SpotHedging ():
         
         # check whether current spot was hedged
         actual_hedging_size : int = self.compute_actual_hedging_size () 
+        log.warning (f'{actual_hedging_size=}')
 
         # check remaining hedging needed
         return int(min_hedged_size if actual_hedging_size  == [] else min_hedged_size - actual_hedging_size )
