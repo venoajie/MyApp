48ea1a8565acc044fb7ffb52d4eb1019001d8593
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-28 12:32:30 +0700

    Update checkEvents.sh

--- src/checkEvents.sh
+++ src/checkEvents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-do python3  synchronizing_files.py
+python3  synchronizing_files.py
 while inotifywait -r -e modify,create,delete,move portfolio/deribit; 
 do python3  synchronizing_files.py
 done
