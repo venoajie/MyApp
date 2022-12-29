9f2ed0aff951c5abe67ecbbe8d8eefa9d1734c8d
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-29 17:55:18 +0700

    Update synchronizing_files.py

--- src/synchronizing_files.py
+++ src/synchronizing_files.py
@@ -177,11 +177,9 @@ class SynchronizingFiles ():
         """
     
         open_order_mgt = await self.open_orders ()
-        len_current_open_orders = open_order_mgt.my_orders_api_basedOn_label_items_qty(label_for_filter)
-        log.warning (open_order_mgt)
-        log.warning (len_current_open_orders)
-        log.warning (len_current_open_orders != [])
-    
+        
+        len_current_open_orders = open_order_mgt.my_orders_api_basedOn_label_items_qty( label_for_filter)
+        
         if len_current_open_orders != [] :
             if len_current_open_orders > 1 :
             
@@ -189,7 +187,6 @@ class SynchronizingFiles ():
                 telegram_bot_sendtext(info,'failed_order')        
                 
                 open_order_id: list = open_order_mgt.my_orders_api_basedOn_label_last_update_timestamps_max_id (label_for_filter) 
-                log.warning (open_order_id)
                 
                 await deribit_get.get_cancel_order_byOrderId (self.connection_url, 
                                                                 self.client_id,
