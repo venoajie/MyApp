83d78c3eddb856d47b11075219b0a921d5ca7992
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-30 19:24:13 +0700

    Update synchronizing_files.py

--- src/synchronizing_files.py
+++ src/synchronizing_files.py
@@ -20,48 +20,40 @@ from configuration import  label_numbering
 from utils import pickling, system_tools, telegram_app, formula, string_modification
 
     
-@dataclass(unsafe_hash=True, slots=True)
-class SynchronizingFiles ():
+async def remove_redundant_data (data) -> list:
+    """
+    """
+    log.error (data)
+    log.info ('AAAA')
+    if isinstance(data, list):
 
+        free_from_duplicates_data = string_modification.remove_redundant_elements (data)
+        
+        log.error (free_from_duplicates_data)
+        
+async def save_data () -> list:
+    """
+    """
+    cleaned_data = await  remove_redundant_data()
+    pickling.replace_data (cleaned_data)
+
+async def cleanUp_data () -> list:
+    """
+    """
+        
+    my_trades_path_open: str = system_tools.provide_path_for_file ('myTrades', self.currency, 'open')
+    my_trades_open: list = pickling.read_data(my_trades_path_open) 
     
-    '''
-    '''       
-
-    data: list
-        
-    async def remove_redundant_data (self) -> list:
-        """
-        """
-        log.error (self.data)
-        if isinstance(self.data, list):
-
-            free_from_duplicates_data = string_modification.remove_redundant_elements (self.data)
-            
-            log.error (free_from_duplicates_data)
-            
-    async def save_data (self) -> list:
-        """
-        """
+    my_path_orders_open: str = system_tools.provide_path_for_file ('orders', self.currency, 'open')
+    my_path_orders_closed: str = system_tools.provide_path_for_file ('orders', self.currency, 'closed')
+    
+    paths = [my_trades_open, my_path_orders_closed, my_path_orders_open]
+    
+    for path in paths:
+        data_from_db = pickling.read_data (path)
         cleaned_data = await self. remove_redundant_data()
-        pickling.replace_data (cleaned_data)
-
-    async def cleanUp_data (self) -> list:
-        """
-        """
-            
-        my_trades_path_open: str = system_tools.provide_path_for_file ('myTrades', self.currency, 'open')
-        my_trades_open: list = pickling.read_data(my_trades_path_open) 
-        
-        my_path_orders_open: str = system_tools.provide_path_for_file ('orders', self.currency, 'open')
-        my_path_orders_closed: str = system_tools.provide_path_for_file ('orders', self.currency, 'closed')
-        
-        paths = [my_trades_open, my_path_orders_closed, my_path_orders_open]
-        
-        for path in paths:
-            data_from_db = pickling.read_data (path)
-            cleaned_data = await self. remove_redundant_data()
-            pickling.replace_data (path, cleaned_data)
-        
+        pickling.replace_data (path, cleaned_data)
+    
                 
 async def main (item):
     
@@ -91,10 +83,12 @@ if __name__ == "__main__":
         
         paths = [my_trades_path_open, my_path_orders_closed, my_path_orders_open]
         log.error (paths)
-
-        asyncio.gather(*[ SynchronizingFiles (item).remove_redundant_data() for item in paths ])  
+        
+        asyncio.gather(*[ remove_redundant_data(item) for item in paths ]) 
         
     except (KeyboardInterrupt, SystemExit):
+
+        
         asyncio.get_event_loop().run_until_complete(main().stop_ws())
 
     except Exception as error:
