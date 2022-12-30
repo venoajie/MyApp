e6dca6297ef0262a545852366637ccfa7f820ec1
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-31 04:32:20 +0700

    Update checkEvents.sh

--- src/checkEvents.sh
+++ src/checkEvents.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 python3  synchronizing_files.py
+python3 hedging_spot.py
+
 while inotifywait -r -e modify,create,delete,move portfolio/deribit; 
-do python3  hedging_spot.py
+    do 
+    python3  synchronizing_files.py
+    python3  hedging_spot.py
 done
