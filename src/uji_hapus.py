a4097efa4e12fa2995188dc4ac18f823b90904bd
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-30 10:36:55 +0700

    Update uji_hapus.py

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -1,11 +1,7 @@
 from collections import OrderedDict
 from utils import string_modification
 
-data_from_db = [
-    {'trade_seq': 12032431, 'trade_id': 'ETH-16795793', 'timestamp': 1672353414082, 'tick_direction': 0, 'state': 'filled', 'self_trade': False, 'risk_reducing': False, 'reduce_only': False, 'profit_loss': 0.0, 'price': 1196.05, 'post_only': True, 'order_type': 'limit', 'order_id': 'ETH-3182524037', 'mmp': False, 'matching_id': None, 'mark_price': 1196.95, 'liquidity': 'M', 'label': 'hedging spot-open-1672353389358', 'instrument_name': 'ETH-PERPETUAL', 'index_price': 1196.23, 'fee_currency': 'ETH', 'fee': 0.0, 'direction': 'sell', 'api': True, 'amount': 10.0}, 
-    {'trade_seq': 12032432, 'trade_id': 'ETH-16795793', 'timestamp': 1672353414082, 'tick_direction': 0, 'state': 'filled', 'self_trade': False, 'risk_reducing': False, 'reduce_only': False, 'profit_loss': 0.0, 'price': 1196.05, 'post_only': True, 'order_type': 'limit', 'order_id': 'ETH-3182524037', 'mmp': False, 'matching_id': None, 'mark_price': 1196.95, 'liquidity': 'M', 'label': 'hedging spot-open-1672353389358', 'instrument_name': 'ETH-PERPETUAL', 'index_price': 1196.23, 'fee_currency': 'ETH', 'fee': 0.0, 'direction': 'sell', 'api': True, 'amount': 10.0}, 
-    {'trade_seq': 12032451, 'trade_id': 'ETH-16795821', 'timestamp': 1672354669320, 'tick_direction': 0, 'state': 'filled', 'self_trade': False, 'risk_reducing': False, 'reduce_only': False, 'profit_loss': -2.044e-05, 'price': 1198.7, 'post_only': True, 'order_type': 'limit', 'order_id': 'ETH-3182583884', 'mmp': False, 'matching_id': None, 'mark_price': 1198.76, 'liquidity': 'M', 'label': 'hedging spot-closed-1672353851674', 'instrument_name': 'ETH-PERPETUAL', 'index_price': 1198.25, 'fee_currency': 'ETH', 'fee': 0.0, 'direction': 'buy', 'api': True, 'amount': 10.0}, 
-    ]
+data_from_db =  [{'timestamp': 1672371033716, 'instrument_name': 'ETH-27JAN23', 'change_id': 3183551047, 'bids': [[1194.5, 1343.0], [1194.4, 3853.0], [1194.2, 1356.0], [1194.1, 1352.0], [1194.0, 1356.0], [1193.8, 1358.0], [1193.7, 9570.0], [1193.5, 9721.0], [1193.2, 16001.0], [1192.7, 399.0]], 'asks': [[1195.0, 495.0], [1195.1, 1361.0], [1195.2, 11066.0], [1195.4, 1360.0], [1195.5, 1361.0], [1195.7, 1360.0], [1195.8, 3867.0], [1195.9, 1370.0], [1196.0, 11731.0], [1196.1, 403.0]]}]
 
 #sum_data_from_db =    sum( [o['amount'] for o in data_from_db  ] )
 #print(f'{sum_data_from_db=}')
