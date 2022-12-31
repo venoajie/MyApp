3437c817822dfde0b87c92dbf38ed0f95b77e342
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-31 17:17:54 +0700

    Update spot_hedging.py

--- src/risk_management/spot_hedging.py
+++ src/risk_management/spot_hedging.py
@@ -194,7 +194,7 @@ class SpotHedging ():
         
         log.debug(f'{myTrades_max_price_pct_minus=} {index_price=} {myTrades_max_price_pct_plus=} ')
         
-        log.debug(f'take_profit {myTrades_max_price_pct_minus < index_price} average_up {myTrades_max_price_pct_plus > index_price} ')
+        log.debug(f'trans.price {myTrades_max_price} take_profit {myTrades_max_price_pct_minus < index_price} average_up {myTrades_max_price_pct_plus > index_price} ')
         
         return {'take_profit': myTrades_max_price_pct_minus < index_price,
                 'label_take_profit':  label_to_send,
