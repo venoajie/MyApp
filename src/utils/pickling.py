c96e87d29b7e182cec946c8b877ecbbe13c5df76
venoajie <59521823+venoajie@users.noreply.github.com>
2022-12-31 07:08:06 +0700

    Update pickling.py

--- src/utils/pickling.py
+++ src/utils/pickling.py
@@ -26,7 +26,7 @@ def check_duplicate_elements (file_name: str)-> None:
     data_from_db: list = read_data (file_name)
    #! 
     log.info (f'{file_name=}')
-    log.info (f'BEFORE {data_from_db=}')
+    log.info (f'BEFORE {len(data_from_db)} {data_from_db=}')
     #!
     free_from_duplicates_data = string_modification.remove_redundant_elements (data_from_db)
     
@@ -34,7 +34,7 @@ def check_duplicate_elements (file_name: str)-> None:
 #! 
     data_from_db: list = read_data (file_name)
     
-    log.warning (f'AFTER {data_from_db=}')
+    log.warning (f'AFTER {len(data_from_db)} {data_from_db=}')
 
 def dump_data_as_list (file_name: str, data: dict, check_duplicates: bool = False)-> None:
 
