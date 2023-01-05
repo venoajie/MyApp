8f17f41de125d8e15bb9e9f4e4a3547c09108c45
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-05 15:29:02 +0700

    Update myTrades_management.py

--- src/portfolio/deribit/myTrades_management.py
+++ src/portfolio/deribit/myTrades_management.py
@@ -43,7 +43,7 @@ class MyTrades ():
             print (sum_closed_trades_in_my_trades_open_sell)
             sum_closed_trades_in_my_trades_open_sell = 0 if sum_closed_trades_in_my_trades_open_sell == [] else sum(sum_closed_trades_in_my_trades_open_sell)
             print (sum_closed_trades_in_my_trades_open_sell)
-            sum_closed_trades_in_my_trades_open_buy = sum([o['amount'] for o in selected_trades if o['direction']=='buy'])
+            sum_closed_trades_in_my_trades_open_buy = ([o['amount'] for o in selected_trades if o['direction']=='buy'])
             print (sum_closed_trades_in_my_trades_open_buy)
             sum_closed_trades_in_my_trades_open_buy = 0 if sum_closed_trades_in_my_trades_open_buy == [] else sum(sum_closed_trades_in_my_trades_open_buy)
             print (sum_closed_trades_in_my_trades_open_buy)
