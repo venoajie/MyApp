61d8fca2c2aeda3860b8e35c89b77c0a1acead29
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-06 21:18:49 +0700

    Update upload_to_remote.sh

--- src/upload_to_remote.sh
+++ src/upload_to_remote.sh
@@ -4,4 +4,4 @@
 
 
 echo "Moving local  files to remote..."
-rclone sync  portfolio/deribit b2:MyAppDeribitTradingBAK
+rclone sync  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppDeribitTradingBAK
