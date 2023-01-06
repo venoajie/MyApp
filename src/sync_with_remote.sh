ea06d82ca26efff057d27a3e04a9a1eaad0caa29
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-07 06:58:53 +0700

    Update sync_with_remote.sh

--- src/sync_with_remote.sh
+++ src/sync_with_remote.sh
@@ -7,19 +7,15 @@ while true; do
         then
                 echo "file exists"
                 echo "Moving local  files to remote..."
-                rclone sync  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppDeribitTradingBAK
+                rclone sync  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppTrading
                         
                 echo "sleep 30 minutes"
                 sleep 30m
 
         else
                 echo "file does not exist"
-                echo "waiting for the file exist. sleep another 15 minutes"
-                sleep 15
-                rclone copy  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppDeribitTradingBAK
+                rclone copy  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppTrading
                 
     fi
 done
 
-
-rclone copy  eth-myTrades-open-recovery-point.pkl b2:MyAppDeribitTradingBAK
