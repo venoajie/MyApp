d1289fa85a8d23a8dc4a12e929e40a5b887bea30
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-06 21:37:31 +0700

    Update upload_to_remote.sh

--- src/upload_to_remote.sh
+++ src/upload_to_remote.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 # https://lovethepenguin.com/linux-check-if-a-file-or-directory-exists-e00cfa672249
 
-if test -f portfolio/deribit/eth-myTrades-open-recovery-point.pkl;
-then
-        echo "file exists"
-        echo "Moving local  files to remote..."
-        rclone sync  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppDeribitTradingBAK
-
-else
-        echo "file does not exist"
         
 fi
 
+while true; do
+
+if test -f portfolio/deribit/eth-myTrades-open-recovery-point.pkl;
+    then
+            echo "file exists"
+            echo "Moving local  files to remote..."
+            rclone sync  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppDeribitTradingBAK
+
+    else
+            echo "file does not exist"
+            
+            
+        fi
+    echo "sleep 10 seconds"
+    sleep 10
+done
 
 
