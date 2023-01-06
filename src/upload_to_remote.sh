e23baede044730e9e3991c7e779c7f6c0976faff
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-06 21:38:23 +0700

    Update upload_to_remote.sh

--- src/upload_to_remote.sh
+++ src/upload_to_remote.sh
@@ -6,19 +6,19 @@ fi
 
 while true; do
 
-if test -f portfolio/deribit/eth-myTrades-open-recovery-point.pkl;
-    then
-            echo "file exists"
-            echo "Moving local  files to remote..."
-            rclone sync  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppDeribitTradingBAK
+    if test -f portfolio/deribit/eth-myTrades-open-recovery-point.pkl;
+        then
+                echo "file exists"
+                echo "Moving local  files to remote..."
+                rclone sync  portfolio/deribit/eth-myTrades-open-recovery-point.pkl b2:MyAppDeribitTradingBAK
 
-    else
-            echo "file does not exist"
+        else
+                echo "file does not exist"
+                
             
-            
-        fi
-    echo "sleep 10 seconds"
-    sleep 10
+    fi
+        echo "sleep 10 seconds"
+        sleep 10
 done
 
 
