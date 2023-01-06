bbd8f1fbbf21ac15819340b5c01c724b05c34ec1
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-06 10:09:54 +0700

    Update apply_hedging_spot.py

--- src/apply_hedging_spot.py
+++ src/apply_hedging_spot.py
@@ -117,7 +117,7 @@ class ApplyHedgingSpot ():
                                                           )
             # redistribute the filtered data into db
             my_trades = myTrades_management.MyTrades (filtered_data_from_my_trades_from_exchange)
-            #log.info(f'DB {my_trades_from_db=}')
+            log.info(f'DB {my_trades_from_db=}')
             #log.info(f'EXC {fetch_my_trades_from_system_from_min_time_stamp_to_now=}')
             #log.info(f'FILTERED {filtered_data_from_my_trades_from_exchange=}')
             
