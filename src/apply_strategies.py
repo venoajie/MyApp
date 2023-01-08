5826e498f67e0551f60188e10599efcea7120bda
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-08 14:16:29 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -557,6 +557,7 @@ class ApplyHedgingSpot ():
                                     my_orders_from_db = await self.get_open_orders_from_exchange()
                                     net_open_orders_open_byAPI_system: int = open_order_mgt_system.my_orders_api_basedOn_label_items_net (label_for_filter)
                                     #log.error (net_open_orders_open_byAPI_system)
+                                    log.debug(f'{net_open_orders_open_byAPI_system=} {net_open_orders_open_byAPI_db=}')
                                     if net_open_orders_open_byAPI_system - net_open_orders_open_byAPI_db != 0:
                                         await self.check_my_orders_consistency (my_orders_from_db, server_time)
                                         
