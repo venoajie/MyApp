a231d32f6a50e5877f886218f1e4ccb120919d2c
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-13 06:55:09 +0700

    Update number_modification.py

--- src/utilities/number_modification.py
+++ src/utilities/number_modification.py
@@ -5,18 +5,16 @@ def net_position (selected_transactions: list)-> float:
     '''
     net sell: negative, net buy: positive
     '''    
-    from loguru import logger as log
 
     if selected_transactions != []:
         
         try:
-            log.critical (selected_transactions)
-            sum_sell = sum([o['amount'] for o in selected_transactions if o['direction']=='sell'  ])
+
+            sum_sell = sum([o['amount'] for o in selected_transactions if o['direction']=='sell'  ]) # sell = + sign
             sum_buy = sum([o['amount'] for o in selected_transactions if o['direction']=='buy'  ])
+            return 0 if selected_transactions == [] else  sum_buy - sum_sell
+        
         except:
-            log.debug (selected_transactions)
-            sum_sell = sum([o['size'] for o in selected_transactions ])
-            sum_buy = sum([o['size'] for o in selected_transactions])
-            
-    return 0 if selected_transactions == [] else  sum_buy - sum_sell
+            return ([o['size'] for o in selected_transactions ])  # sell = (-) sign
+    
 
