a19b2ecf7607ad6b74d6162efb6cd1bf06d7e409
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-14 15:22:39 +0700

    Update myTrades_management.py

--- src/portfolio/deribit/myTrades_management.py
+++ src/portfolio/deribit/myTrades_management.py
@@ -103,7 +103,7 @@ class MyTrades ():
                     
                     #!HAPUS
                     my_trades_open = pickling.read_data(my_trades_path_open)  
-                    log.debug (my_trades_open)
+                    log.warning (my_trades_open)
                     #!HAPUS
                     
                 if 'closed' in label_id:
