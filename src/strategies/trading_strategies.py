ade8ae03c85a62c29faf6ce891381cf308bb6862
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-15 21:32:46 +0700

    Update trading_strategies.py

--- src/strategies/trading_strategies.py
+++ src/strategies/trading_strategies.py
@@ -68,6 +68,7 @@ class RunningStrategies ():
             my_trades_buy = self.my_trades_direction () ['buy'] 
             
             for my_trades in self.my_trades_open:
+                log.warning (my_trades)
                 
                 price  = [o['price']  for o in my_trades] [0]
                 label = self.strategy_attributes ['label_strategy']      
