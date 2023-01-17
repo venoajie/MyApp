7a620c60271128f3536c7037131b35598311ffcc
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-18 06:27:28 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -522,7 +522,7 @@ class ApplyHedgingSpot ():
                 # my trades data
                 my_trades_open: list = reading_from_database ['my_trades_open']
                 my_trades_open_mgt: list = myTrades_management.MyTrades (my_trades_open)
-                #log.info (my_trades_open)
+                log.info (my_trades_open)
                 
                 # fetch instruments data
                 instruments = reading_from_database ['instruments']
