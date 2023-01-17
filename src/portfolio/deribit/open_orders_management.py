2723158d2ab21e1e2eceeed9c4ec979427650f46
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-17 12:09:04 +0700

    Update open_orders_management.py

--- src/portfolio/deribit/open_orders_management.py
+++ src/portfolio/deribit/open_orders_management.py
@@ -4,6 +4,13 @@
 from dataclassy import dataclass
 from loguru import logger as log
 
+
+def catch_error (error, idle: int = None) -> list:
+    """
+    """
+    from utilities import system_tools
+    system_tools.catch_error_message(error, idle)
+
 @dataclass(unsafe_hash=True, slots=True)
 class MyOrders ():
 
@@ -161,50 +168,53 @@ class MyOrders ():
         
         my_path_orders_open = system_tools.provide_path_for_file ('orders', currency, 'open')
         #log.error (self.my_orders)
-        
-        if self.my_orders:
-            
-            for order in self.my_orders:
-                log.warning ('trade_seq' not in order)
-                if 'trade_seq'not in order:
-                    
-                    #log.error (f'{self.my_orders=}')
-                    log.warning (f'{order=}')
+        try:
+                
+            if self.my_orders:
+                
+                for order in self.my_orders:
+                    log.warning ('trade_seq' not in order)
+                    if 'trade_seq'not in order:
                         
-                    order_id= order ['order_id']
-                    order_state = order ['order_state']
-                    
-                    my_path_orders_else = system_tools.provide_path_for_file ('orders', currency, order_state)
-                    open_orders_open = pickling.read_data (my_path_orders_open) 
-                    
-                    if order_state == 'open':
-                        log.error ('ORDER_STATE OPEN')
-                        log.info (f'{order=}')
-                        
-                        pickling.append_and_replace_items_based_on_qty (my_path_orders_open, order, 1000, True)
-                        
-                    else:
-                        log.critical ('ORDER_STATE ELSE')
-                        log.info (f'{order=}')
-                        log.info (f'{open_orders_open=}')
-                        item_in_open_orders_open_with_same_id =  [o for o in open_orders_open if o['order_id'] == order_id ] 
-                        item_in_open_orders_open_with_diff_id =  [o for o in open_orders_open if o['order_id'] != order_id ] 
-                        log.debug (f'{item_in_open_orders_open_with_same_id=}')
-                        
-                        pickling.append_and_replace_items_based_on_qty (my_path_orders_else, order, 1000, True)
-                        #result_example = [
-                        #    {'web': False, 'time_in_force': 'good_til_cancelled', 'risk_reducing': False, 'replaced': False, 'reject_post_only': False, #'reduce_only': False, 'profit_loss': 0.0, 'price': 1547.6, 'post_only': True, 'order_type': 'limit', 'order_state': 'filled', #'order_id': 'ETH-3249516850', 'mmp': False, 'max_show': 53.0, 'last_update_timestamp': 1673675558839, 'label': #'hedgingSpot-open-1673675540', 'is_liquidation': False, 'instrument_name': 'ETH-PERPETUAL', 'filled_amount': 53.0, 'direction': #'sell', 'creation_timestamp': 1673675541623, 'commission': 0.0, 'average_price': 1547.6, 'api': True, 'amount': 53.0}, 
-                        #    {'web': False, 'time_in_force': 'good_til_cancelled', 'risk_reducing': False, 'replaced': False, 'reject_post_only': False, #'reduce_only': False, 'profit_loss': 0.0, 'price': 1547.6, 'post_only': True, 'order_type': 'limit', 'order_state': 'open', #'order_id': 'ETH-3249516850', 'mmp': False, 'max_show': 53.0, 'last_update_timestamp': 1673675541623, 'label': #'hedgingSpot-open-1673675540', 'is_liquidation': False, 'instrument_name': 'ETH-PERPETUAL', 'filled_amount': 0.0, 'direction': 'sell', #'creation_timestamp': 1673675541623, 'commission': 0.0, 'average_price': 0.0, 'api': True, 'amount': 53.0}
-                        #    ]
-                        
-                        if item_in_open_orders_open_with_same_id != []:
-
-                            pickling.append_and_replace_items_based_on_qty (my_path_orders_else, item_in_open_orders_open_with_same_id, 100000, True)
+                        #log.error (f'{self.my_orders=}')
+                        log.warning (f'{order=}')
                             
-                        pickling.replace_data (my_path_orders_open, item_in_open_orders_open_with_diff_id, True)
+                        order_id= order ['order_id']
+                        order_state = order ['order_state']
+                        
+                        my_path_orders_else = system_tools.provide_path_for_file ('orders', currency, order_state)
+                        open_orders_open = pickling.read_data (my_path_orders_open) 
+                        
+                        if order_state == 'open':
+                            log.error ('ORDER_STATE OPEN')
+                            log.info (f'{order=}')
+                            
+                            pickling.append_and_replace_items_based_on_qty (my_path_orders_open, order, 1000, True)
+                            
+                        else:
+                            log.critical ('ORDER_STATE ELSE')
+                            log.info (f'{order=}')
+                            log.info (f'{open_orders_open=}')
+                            item_in_open_orders_open_with_same_id =  [o for o in open_orders_open if o['order_id'] == order_id ] 
+                            item_in_open_orders_open_with_diff_id =  [o for o in open_orders_open if o['order_id'] != order_id ] 
+                            log.debug (f'{item_in_open_orders_open_with_same_id=}')
+                            
+                            pickling.append_and_replace_items_based_on_qty (my_path_orders_else, order, 1000, True)
+                            #result_example = [
+                            #    {'web': False, 'time_in_force': 'good_til_cancelled', 'risk_reducing': False, 'replaced': False, 'reject_post_only': False, #'reduce_only': False, 'profit_loss': 0.0, 'price': 1547.6, 'post_only': True, 'order_type': 'limit', 'order_state': 'filled', #'order_id': 'ETH-3249516850', 'mmp': False, 'max_show': 53.0, 'last_update_timestamp': 1673675558839, 'label': #'hedgingSpot-open-1673675540', 'is_liquidation': False, 'instrument_name': 'ETH-PERPETUAL', 'filled_amount': 53.0, 'direction': #'sell', 'creation_timestamp': 1673675541623, 'commission': 0.0, 'average_price': 1547.6, 'api': True, 'amount': 53.0}, 
+                            #    {'web': False, 'time_in_force': 'good_til_cancelled', 'risk_reducing': False, 'replaced': False, 'reject_post_only': False, #'reduce_only': False, 'profit_loss': 0.0, 'price': 1547.6, 'post_only': True, 'order_type': 'limit', 'order_state': 'open', #'order_id': 'ETH-3249516850', 'mmp': False, 'max_show': 53.0, 'last_update_timestamp': 1673675541623, 'label': #'hedgingSpot-open-1673675540', 'is_liquidation': False, 'instrument_name': 'ETH-PERPETUAL', 'filled_amount': 0.0, 'direction': 'sell', #'creation_timestamp': 1673675541623, 'commission': 0.0, 'average_price': 0.0, 'api': True, 'amount': 53.0}
+                            #    ]
+                            
+                            if item_in_open_orders_open_with_same_id != []:
 
-        else:
-            pickling.replace_data (my_path_orders_open, [], True)
-                    
-    
+                                pickling.append_and_replace_items_based_on_qty (my_path_orders_else, item_in_open_orders_open_with_same_id, 100000, True)
+                                
+                            pickling.replace_data (my_path_orders_open, item_in_open_orders_open_with_diff_id, True)
+
+            else:
+                pickling.replace_data (my_path_orders_open, [], True)
+                        
+        except Exception as error:
+            catch_error (error)
+        
     
