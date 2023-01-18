48ba99e41c60efa98f970326f741279ec4c64dfe
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-18 11:32:30 +0700

    Update entries_exits.py

--- src/strategies/entries_exits.py
+++ src/strategies/entries_exits.py
@@ -2,39 +2,6 @@
 # -*- coding: utf-8 -*-
 
 strategies = [
-    {
-    'strategy': 'supplyDemandShort15',
-    'instrument': ['PERPETUAL'],
-    'time_frame': 900,
-    'side': 'sell',
-    'entry_price': 1544,
-    'take_profit_usd': 1543.5,
-    'take_profit_pct': 1/100,
-    'quantity_discrete': 15,
-    'cut_loss_usd': 1544.5,
-    'cut_loss_pct': (1/100)/2,
-    'averaging': 15,
-    'halt_minute_before_reorder': 60, 
-    'equity_risked_usd': 60, 
-    'equity_risked_pct': 1/100
-    },
-    
-    {
-    'strategy': 'supplyDemandLong15',
-    'instrument': ['PERPETUAL'],
-    'time_frame': 900,
-    'side': 'buy',
-    'entry_price': 1542,
-    'take_profit_usd': 1542.5,
-    'take_profit_pct': 1/100,
-    'quantity_discrete': 900,
-    'cut_loss_usd': 1543.5,
-    'cut_loss_pct': (1/100)/2,
-    'averaging': 15,
-    'halt_minute_before_reorder': 60, 
-    'equity_risked_usd': 60, 
-    'equity_risked_pct': 1/100
-    },
     
     {
     'strategy': 'hedgingSpot',
