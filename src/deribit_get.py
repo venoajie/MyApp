6a839d1fa9ba1b706cfc827f3854b9863743d5a1
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-18 11:05:10 +0700

    Update deribit_get.py

--- src/deribit_get.py
+++ src/deribit_get.py
@@ -392,9 +392,6 @@ async def get_positions (connection_url: str, client_id, client_secret, currency
         
     params =  {"currency": currency}
     endpoint: str = 'private/get_positions'
-    print (connection_url)
-    print (client_id)
-    print (client_secret)
     result = await main(
             endpoint=endpoint,
             params=params,
