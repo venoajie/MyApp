65fea5f08b812d17d3638fda67dd812e1b01a8e4
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-18 09:56:16 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -859,7 +859,7 @@ async def main ():
     client_id: str = parse_dotenv() ['client_id']
     client_secret: str = parse_dotenv() ['client_secret']
     connection_url: str = 'wss://www.deribit.com/ws/api/v2'
-    
+    #
     try:
 
         syn = ApplyHedgingSpot (
