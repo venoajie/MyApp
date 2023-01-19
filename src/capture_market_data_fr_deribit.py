d820461c38e30a1fa55412caca8b59e564b25b4e
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-19 11:02:20 +0700

    Update capture_market_data_fr_deribit.py

--- src/capture_market_data_fr_deribit.py
+++ src/capture_market_data_fr_deribit.py
@@ -216,7 +216,7 @@ class StreamMarketData:
                                 ticker_instrument: list = pickling.read_data(my_path_ticker) 
                                 log.critical (ticker_instrument) 
                                 instrument_name = ticker_instrument [0]['instrument_name']
-                                instrument: list = [o for o in ticker_instrument if o['instrument_name'] == instrument_name] 
+                                instrument: list = [o for o in instruments_kind if o['instrument_name'] == instrument_name] 
                                 log.critical (instrument) 
                                 tickers = futures_analysis.combining_individual_futures_analysis (index_price [0]['price'], 
                                                                                                   instrument, 
