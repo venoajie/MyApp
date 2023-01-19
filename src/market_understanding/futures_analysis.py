0cee3d3e031f1b6a249cc22b7ddff57315c752ef
venoajie <59521823+venoajie@users.noreply.github.com>
2023-01-19 16:07:51 +0700

    Update futures_analysis.py

--- src/market_understanding/futures_analysis.py
+++ src/market_understanding/futures_analysis.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from utilities import pickling, system_tools, time_modification
-from loguru import logger as log
+from utilities import time_modification
 
 def combining_individual_futures_analysis (index_price: float, 
                                 future: dict,
@@ -27,7 +26,7 @@ def combining_individual_futures_analysis (index_price: float,
         remaining_active_time: int = future - now_time_unix 
         remaining_active_time_hours: float = (remaining_active_time/(60000))/60
         delta_price: float = (mark_price - index_price)
-        ratio_price_to_perp: float = delta_price / index_price
+        ratio_price_to_index: float = delta_price / index_price
 
         # combining current and next coins rate
         dicttemp = {}                
@@ -35,7 +34,7 @@ def combining_individual_futures_analysis (index_price: float,
                     'mark_price': mark_price,
                     'with_rebates': instruments_with_rebates,
                     'margin_usd': margin,
-                    'ratio_to_perpetual': 0 if 'PERPETUAL' in instrument else ratio_price_to_perp,
+                    'ratio_price_to_index':  ratio_price_to_index,
                     'margin_pct': margin_pct,
                     'remaining_active_time_in_hours': remaining_active_time_hours,
                     'market_expectation': 'contango' if margin > 0 else ('backwardation' if margin < 0 else 'neutral')}                    
@@ -46,48 +45,4 @@ def combining_individual_futures_analysis (index_price: float,
 
         print (error)
         
-    return futures
-
-def combining_futures_analysis (index_price: float, 
-                                list_instruments:list,
-                                server_time) -> dict:
-
-    '''
-    '''   
-    
-    futures =[]
-    
-    now_time = time_modification.convert_time_to_utc()['utc_now']
-    now_time_unix = time_modification.convert_time_to_unix (now_time) 
-    print (server_time)
-    print (now_time_unix)
-    for future in list_instruments:
-
-        instrument = future ['instrument_name']
-        my_path_ticker: str = system_tools.provide_path_for_file ('ticker', instrument) 
-        ticker =  pickling.read_data(my_path_ticker)
-        ticker =  ticker [0]
-        mark_price = ticker ['mark_price']
-        
-        # obtain funding next rate based on individual coin
-        margin: float  = mark_price - index_price  
-        margin_pct: float  =  margin / index_price
-        instruments_with_rebates: float = future ['maker_commission'] < 0
-        
-        future: int = future ['expiration_timestamp']
-        remaining_active_time: int = future - server_time 
-        remaining_active_time_hours: float = (remaining_active_time/(60000))/60
-
-        # combining current and next coins rate
-        dicttemp = {}                
-        dicttemp = {'instrument_name': instrument,
-                    'mark_price': mark_price,
-                    'with_rebates': instruments_with_rebates,
-                    'margin_usd': margin,
-                    'margin_pct': margin_pct,
-                    'remaining_active_time_in_hours': remaining_active_time_hours,
-                    'market_expectation': 'contango' if margin > 0 else ('backwardation' if margin < 0 else 'neutral')}                    
-        
-        data_future = futures.append(dicttemp.copy())        
-                
     return futures
