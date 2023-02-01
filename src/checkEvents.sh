ee87e65ea6d24f3f1ff416cf4e44acfc784b54d7
venoajie <59521823+venoajie@users.noreply.github.com>
2023-02-01 19:39:17 +0700

    Update checkEvents.sh

--- src/checkEvents.sh
+++ src/checkEvents.sh
@@ -3,6 +3,5 @@ python3 apply_strategies.py
 
 while inotifywait -r -e modify,create,delete,move portfolio/deribit; 
     do 
-    python3  synchronizing_files.py
     python3  apply_strategies.py
 done
