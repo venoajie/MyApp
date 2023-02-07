356a32c78e175517d105560a28aba11bc42f507b
venoajie <59521823+venoajie@users.noreply.github.com>
2023-02-07 18:37:31 +0700

    Update check_data_integrity.py

--- src/risk_management/check_data_integrity.py
+++ src/risk_management/check_data_integrity.py
@@ -194,10 +194,12 @@ async def main_enforce_my_trade_db_integrity (
 
     '''
     '''       
-    log.critical ('source for distribute_trade_transaction')
-    log.warning (my_trades_open_from_db)
+    log.critical (len(my_trades_open_from_db))
+    log.critical ('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+   # log.warning (my_trades_open_from_db)
     my_trades_open_mgt: object = myTrades_management.MyTrades (my_trades_open_from_db)     
     my_trades_open_mgt.distribute_trade_transaction (currency)
+    log.critical ('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
             
     trade_integrity =  CheckTradeIntegrity (currency,
                                            positions_from_get,
