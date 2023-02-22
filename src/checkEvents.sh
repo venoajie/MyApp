2a6990eea6d8b326dfbd9dc336fed94217320e96
venoajie <59521823+venoajie@users.noreply.github.com>
2023-02-22 20:44:22 +0700

    Update checkEvents.sh

--- src/checkEvents.sh
+++ src/checkEvents.sh
@@ -3,6 +3,6 @@ python3 apply_strategies.py
 
 while inotifywait -r -e modify,create,delete,move portfolio/deribit; 
     do 
-    python3  apply_strategies.py
     rsync -partial -z -e 'ssh -p 22' portfolio/deribit/eth-myTrades-open-recovery-point.pkl ubuntu@instance-20230101-2309:/live/MyApp/local_recoveries
+    python3  apply_strategies.py
 done
