ac7c51f13b7b62a0cc8ffb5e10f0135dc8189d6a
venoajie <59521823+venoajie@users.noreply.github.com>
2023-02-24 08:24:13 +0700

    Update checkEvents.sh

--- src/checkEvents.sh
+++ src/checkEvents.sh
@@ -3,6 +3,7 @@ python3 apply_strategies.py
 
 while inotifywait -r -e modify,create,delete,move portfolio/deribit; 
     do 
-    rsync -partial -z -e 'ssh -p 22' portfolio/deribit/ live/MyApp/local_recoveries
+    
     python3  apply_strategies.py
+    rsync -partial -z   portfolio/deribit/ ~/live/MyApp/local_recoveries/
 done
