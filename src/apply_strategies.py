6431af9809eed2f93f420d79869190fcd62aa432
venoajie <59521823+venoajie@users.noreply.github.com>
2023-02-25 22:31:05 +0700

    update trade open

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -634,7 +634,7 @@ class ApplyHedgingSpot ():
                 
                 open_order_mgt_filed_status_filed = open_order_mgt_filed.open_orders_status ('filled')
                 
-                log.info (my_trades_open)
+                #log.info (my_trades_open)
                 #! CHECK BALANCE AND TRANSACTIONS INTEGRITY
                 await self.check_open_orders_integrity (open_orders_open_byAPI,
                                                         open_orders_from_sub_account_get
