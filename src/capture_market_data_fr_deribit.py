67d0fe95cb1f51096a677ee6270149ed9a265ed9
venoajie <59521823+venoajie@users.noreply.github.com>
2023-02-26 20:32:06 +0700

    Update capture_market_data_fr_deribit.py

--- src/capture_market_data_fr_deribit.py
+++ src/capture_market_data_fr_deribit.py
@@ -299,7 +299,7 @@ class StreamMarketData:
                 log.warning(f'ticker_fr_snapshot BBBB {ticker_fr_snapshot}' )
                 
             else:
-                ticker_fr_snapshot: list = pickling.read_data(my_path_ticker)
+                #ticker_fr_snapshot: list = pickling.read_data(my_path_ticker)
                 log.error(f'ticker_fr_snapshot 1 {ticker_fr_snapshot}' )
                 
                 for item in data_orders:
