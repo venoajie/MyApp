167d06336270bed9781f52473e8d1ea1e16a903d
venoajie <59521823+venoajie@users.noreply.github.com>
2023-02-26 20:36:40 +0700

    Update capture_market_data_fr_deribit.py

--- src/capture_market_data_fr_deribit.py
+++ src/capture_market_data_fr_deribit.py
@@ -286,8 +286,8 @@ class StreamMarketData:
         
         try:
             log.critical(instrument.upper())
-            ticker_fr_snapshot: list = pickling.read_data(my_path_ticker)
-            log.error(f'ticker_fr_snapshot AAA {ticker_fr_snapshot}' )
+            ticker: list = pickling.read_data(my_path_ticker)
+            log.error(f'ticker_fr_snapshot AAA {ticker}' )
             log.error(f'read {pickling.read_data(my_path_ticker)}' )
             log.info(f' order {data_orders}')
                 
@@ -300,17 +300,17 @@ class StreamMarketData:
                 
             else:
                 #ticker_fr_snapshot: list = pickling.read_data(my_path_ticker)
-                log.error(f'ticker_fr_snapshot 1 {ticker_fr_snapshot}' )
+                log.error(f'ticker_fr_snapshot 1 {ticker}' )
                 
                 for item in data_orders:
                         
-                    ticker_fr_snapshot [0][item] = data_orders [item]
+                    ticker [0][item] = data_orders [item]
                     log.info(f'data_orders ITEM {data_orders [item]}' )
                 
-                    pickling.replace_data(my_path_ticker, ticker_fr_snapshot)              
+                    pickling.replace_data(my_path_ticker, ticker)              
 
-                ticker_fr_snapshot: list = pickling.read_data(my_path_ticker)
-                log.error(f'ticker_fr_snapshot 2 {ticker_fr_snapshot}' )
+                ticker: list = pickling.read_data(my_path_ticker)
+                log.error(f'ticker_fr_snapshot 2 {ticker}' )
                 
         except Exception as error:
             system_tools.catch_error_message (
