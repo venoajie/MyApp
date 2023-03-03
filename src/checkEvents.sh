fa4c97f4bcc285bce702a134e56f15aa67c159ba
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-03 19:57:27 +0700

    Update checkEvents.sh

--- src/checkEvents.sh
+++ src/checkEvents.sh
@@ -3,7 +3,7 @@ python3 apply_strategies.py
 rsync -partial -z   databases/exchanges/deribit/transactions/ ~/live/MyApp/local_recoveries/
 rsync -partial -z   databases/exchanges/deribit/portfolio/ ~/live/MyApp/local_recoveries/
 
-while inotifywait -r -e modify,create,delete,move databases/transactions/deribit; 
+while inotifywait -r -e modify,create,delete,move databases/exchanges/deribit/transactions; 
     do 
     
     rsync -partial -z   databases/exchanges/deribit/transactions/ ~/live/MyApp/local_recoveries/
