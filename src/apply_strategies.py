dacf937b7270d6df84dea879a3e86b35ab7ca368
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-04 13:30:05 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -668,9 +668,9 @@ class ApplyHedgingSpot ():
                             log.warning (open_str_sell)
                             log.warning (open_str_buy)
                             
-                            log.warning (strategy)
-                            log.warning (my_trades_open)
-                            my_trades_strategy = [o for o in my_trades_open['strategy'] if strategy in o['label'] ]
+                            log.warning (strategy['strategy'] )
+                            #log.warning (my_trades_open)
+                            my_trades_strategy = [o for o in my_trades_open if strategy['strategy'] in o['label'] ]
                             log.warning (my_trades_strategy)
                             if my_trades_strategy != []:
                                 for my_trade in my_trades_open:
