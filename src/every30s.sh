c5fa5b62f9a72b153a60e8796354dc312ba2a4b2
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-07 08:41:50 +0700

    Create every30s.sh

---
+++ src/every30s.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://lovethepenguin.com/linux-check-if-a-file-or-directory-exists-e00cfa672249
+
+while true; do
+
+    echo 'xxxxxxxxxxxxxxxx'
+    python3 apply_strategies.py
+    sleep 30
+                
+    fi
+done
+
