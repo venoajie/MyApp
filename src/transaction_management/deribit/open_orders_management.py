d910e0c32e565ca0325d5728d8d557b2d45f7396
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-13 18:17:11 +0700

    Update open_orders_management.py

--- src/transaction_management/deribit/open_orders_management.py
+++ src/transaction_management/deribit/open_orders_management.py
@@ -498,12 +498,16 @@ class MyOrders ():
                                                                                         basic_strategy,
                                                                                         'stop_market')
                 len_open_order_label_strategy_type_market = open_order_label_strategy_type_market ['len_transactions']
+                
                 order_type_market =  False if 'hedgingSpot' in label_basic_strategy \
                     else True if len_open_order_label_strategy_type_market == [] \
                         else len_open_order_label_strategy_type_market < 1
-            
+                log.error (f'len_open_order_label_strategy_type_limit {len_open_order_label_strategy_type_limit}')
+                log.error (f'len_open_order_label_strategy_type_market {len_open_order_label_strategy_type_market}')
             return {'remain_main_orders': remain_main_orders,
                     'remain_exit_orders': remain_exit_orders,
+                    'no_open_order_outstanding': len_open_order_label_strategy_type_limit == [] \
+                        and len_open_order_label_strategy_type_market ==[],
                     #'proforma_size_ok': proforma_size < max_size,
                     'order_type_market': order_type_market,
                     'order_type_limit': True if len_open_order_label_strategy_type_limit == [] \
