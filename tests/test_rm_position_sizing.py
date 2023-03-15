e9ee3371fa3b5bc155ef135145ff16b761fdfb7f
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-15 16:31:07 +0700

    Update test_rm_position_sizing.py

--- tests/test_rm_position_sizing.py
+++ tests/test_rm_position_sizing.py
@@ -6,3 +6,20 @@ def test_pos_sizing():
     assert position_sizing.pos_sizing(target_price=90, entry_price=100, capital=1000, pct_loss = .5/100) == 45
     assert position_sizing.pos_sizing(target_price=100, entry_price=90, capital=1000, pct_loss = 1/100) == 100
     assert position_sizing.pos_sizing(target_price=90, entry_price=100, capital=1000, pct_loss = 1/100) == 90
+
+    
+def test_compute_delta  ():
+    
+    notional = 100
+    long = 0
+    short = -100
+    compute_delta =  position_sizing.compute_delta(notional, long, short) 
+    assert compute_delta == 0
+    
+def test_compute_leverage  ():
+    
+    notional = 100
+    long = 0
+    short = -100
+    compute_delta =  position_sizing.compute_leverage(notional, long, short) 
+    assert compute_delta == 1
