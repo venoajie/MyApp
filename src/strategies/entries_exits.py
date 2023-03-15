3fcdea9bf5ac26231d732199f760dc442398d3d0
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-16 05:59:48 +0700

    Update entries_exits.py

--- src/strategies/entries_exits.py
+++ src/strategies/entries_exits.py
@@ -1,41 +1,16 @@
 # -*- coding: utf-8 -*-
+
+'''
+For strategy with many derivatives name (a/b/c):
+    good strategy name: 1) 'supplyDemandShort60A'/'supplyDemandShort60B'/supplyDemandShort60C'
+    bad strategy name: 'supplyDemandShort60'
+    why? because in sorting process, supplyDemandShort60 means all strategy 
+        contains 'supplyDemandShort60', not exclusively 'supplyDemandShort60'
+'''
+
 equity_risked_pct_default =  1/100/4
 
 strategies = [
-    {
-    'strategy': 'supplyDemandShort60',
-    'instrument': ['PERPETUAL'],
-    'time_frame': 3600,
-    'side': 'sell',
-    'entry_price': 1675,
-    'invalidation_entry_price': 1680,
-    'take_profit_usd': 1640,
-    'take_profit_pct': 1/100,
-    'quantity_discrete': 15,
-    'cut_loss_usd': 1720,
-    'cut_loss_pct': (1/100)/2,
-    'averaging': 15,
-    'halt_minute_before_reorder': 60, 
-    'equity_risked_usd': 60, 
-    'equity_risked_pct': equity_risked_pct_default
-    },
-    {
-    'strategy': 'supplyDemandLong60',
-    'instrument': ['PERPETUAL'],
-    'time_frame': 3600,
-    'side': 'buy',
-    'entry_price': 1635,
-    'invalidation_entry_price': 1630,
-    'take_profit_usd': 1670,
-    'take_profit_pct': 1/100,
-    'quantity_discrete': 15,
-    'cut_loss_usd': 1570,
-    'cut_loss_pct': (1/100)/2,
-    'averaging': 15,
-    'halt_minute_before_reorder': 60, 
-    'equity_risked_usd': 60, 
-    'equity_risked_pct': equity_risked_pct_default
-    },
     
     {
     'strategy': 'supplyDemandLong60A',
