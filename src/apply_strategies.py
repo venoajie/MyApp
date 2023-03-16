ae3b51516f872b0f68ec932c7f830981c09e9cad
venoajie <venoajie@users.noreply.github.com>
2023-03-16 00:47:41 +0000

    Format code with black

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -697,15 +697,15 @@ class ApplyHedgingSpot:
                     o for o in my_trades_open if o["direction"] == "sell"
                 ]
                 if "hedgingSpot" in label_strategy:
-                    log.warning (my_trade_sell_open)
+                    log.warning(my_trade_sell_open)
                 my_trade_sell_open_label_strategy = [
                     o for o in my_trade_sell_open if label_strategy in o["label"]
                 ]
-                
+
                 if "hedgingSpot" in label_strategy:
-                    log.warning (label_strategy)
-                    log.warning (my_trade_sell_open)
-                    log.info (my_trade_sell_open_label_strategy)
+                    log.warning(label_strategy)
+                    log.warning(my_trade_sell_open)
+                    log.info(my_trade_sell_open_label_strategy)
 
             if "hedgingSpot" in label_strategy:
                 log.critical("HEDGING SPOT MAIN ORDER")
