37d5d46bf62dd43f5519f90adaab8871d755a38f
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-17 16:02:59 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -530,10 +530,8 @@ class ApplyHedgingSpot:
             log.critical(determine_size_and_side)
             remain_main_orders = abs(determine_size_and_side["main_orders_qty"])
             remain_exit_orders = abs(determine_size_and_side["exit_orders_limit_qty"])
-            no_limit_open_order_outstanding = determine_size_and_side[
-                "no_limit_open_order_outstanding"
-            ]
-
+            
+            no_limit_open_order_outstanding = open_orders_strategy_limit["len_transactions"]
             #!#################################
             # log.debug (strategy_attr)
 
