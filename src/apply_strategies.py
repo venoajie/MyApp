88423e710a5b2e015768bb453825998bba29c25c
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-18 13:28:30 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -844,9 +844,7 @@ class ApplyHedgingSpot:
                         log.error(f"exit_order_allowed {exit_order_allowed}")
                         
                         if exit_order_allowed ['exit_orders_limit_qty'] not in none_data:
-                            
-                            log.warning(f"exit_orders_limit_type")
-                        
+                                                    
                             if "hedgingSpot" in strategy_attr["strategy"]:
                                 log.warning(f"exit_orders_limit_type")
                             else :
