87847cc4a11792d8436c6f34183386e29eb52b19
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-18 11:56:54 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -545,11 +545,11 @@ class ApplyHedgingSpot:
         open_trade_hedging_price_max = max([o['price'] for o in open_trade_hedging  ])
         open_trade_hedging_selected = ([o  for o in open_trade_hedging if o['price'] == open_trade_hedging_price_max])
         #log.info (open_trade_hedging)
-        log.info (open_trade_hedging_price_max)
-        log.info (open_trade_hedging_selected)
-        log.warning ([o['label'] for o in open_trade_hedging_selected ][0])
-        log.info (get_strategy_int)
-        log.debug (get_strategy_int in [o['label'] for o in open_trade_hedging_selected ][0])
+        #log.info (open_trade_hedging_price_max)
+        #log.info (open_trade_hedging_selected)
+        #log.warning ([o['label'] for o in open_trade_hedging_selected ][0])
+        #log.info (get_strategy_int)
+        #log.debug (get_strategy_int in [o['label'] for o in open_trade_hedging_selected ][0])
         
         if open_trade_hedging_selected !=[]:
             
