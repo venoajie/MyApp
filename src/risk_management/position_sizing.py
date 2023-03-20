505d6539dd544392e44ad046a5a704435559168e
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-20 21:51:37 +0700

    Update position_sizing.py

--- src/risk_management/position_sizing.py
+++ src/risk_management/position_sizing.py
@@ -65,41 +65,95 @@ def pos_sizing(
         )
     )
 
+def compute_my_trade_based_on_side(my_trades_open: list) -> float:
+    """
+
+    Args:
+        my_trades_open (list)
+
+    Returns:
+        dict
+
+    """
+
+    total_long = (
+        0
+        if my_trades_open == []
+        else sum([o["amount"] for o in my_trades_open if o["direction"] == "buy"])
+    )
+
+    total_short = (
+        0
+        if my_trades_open == []
+        else sum([o["amount"] for o in my_trades_open if o["direction"] == "sell"])
+        * -1
+    )
+        
+    return {
+            "total_long": total_long,
+            "total_short": total_short
+            }
+
 
 def compute_delta(notional: float, total_long_qty: int, total_short_qty: int) -> float:
     """
     Compute delta for current position
-
     Args:
         notional (float)
         total_short_qty (int)
         total_short_qty (int)
-
     Returns:
         float
-
     Reference:
         https://
-
     """
     return (notional + total_long_qty + total_short_qty) / notional
-
 
 def compute_leverage(
     notional: float, total_long_qty: int, total_short_qty: int
 ) -> float:
     """
     Compute leverage for current position
-
     Args:
         notional (float)
         total_short_qty (int)
         total_short_qty (int)
+    Returns:
+        float
+    Reference:
+        https://
+    """
+    return (total_long_qty + abs(total_short_qty)) / notional
+
+def compute_position_leverage_and_delta(notional: float, my_trades_open: list) -> list:
+    """
+    Combining result of the computation:
+    - Delta for current position
+    - Leverage for current position
+
+    Args:
+        my_trades_open (list)
 
     Returns:
         float
 
     Reference:
         https://
+
     """
-    return (total_long_qty + abs(total_short_qty)) / notional
+    trade_based_on_side = compute_my_trade_based_on_side(my_trades_open) 
+    total_long_qty = trade_based_on_side['total_long']
+    total_short_qty = trade_based_on_side['total_short']
+    return {'delta': compute_delta(notional, total_long_qty, total_short_qty),
+            'leverage': compute_leverage(notional, total_long_qty, total_short_qty)}
+    
+def sizing_for_perpetual_grid(notional, pct_daily_profit_target, pct_profit_per_transaction, pct_capital_risk) -> float:
+    """
+    """
+    daily_target_in_usd = notional * pct_daily_profit_target
+    capital_risked = max (1, notional * pct_capital_risk)
+    transaction_frequency = daily_target_in_usd/(pct_profit_per_transaction * capital_risked)
+    return transaction_frequency
+
+    
+
