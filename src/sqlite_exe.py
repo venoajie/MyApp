2af1d13826a1e858f47d474b880d650c586f3061
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 06:15:28 +0700

    Update sqlite_exe.py

--- src/sqlite_exe.py
+++ src/sqlite_exe.py
@@ -12,12 +12,72 @@ def catch_error(error, idle: int = None) -> list:
 if __name__ == "__main__":
     try:
         sqlite_management.create_dataBase_sqlite('databases/trading.sqlite3')
-        sqlite_management.create_table_mytrades()
+
+        sqlite_management.create_tables()
         params= [{'trade_seq': 119459281, 'trade_id': 'ETH-162634254', 'timestamp': 1678610180143, 'tick_direction': 0, 'state': 'filled', 'self_trade': False, 'risk_reducing': False, 'reduce_only': False, 'profit_loss': 0.0, 'price': 1473.05, 'post_only': True, 'order_type': 'limit', 'order_id': 'ETH-32205761779', 'mmp': False, 'matching_id': None, 'mark_price': 1472.79, 'liquidity': 'M', 'label': 'hedgingSpot-open-1678610144572', 'instrument_name': 'ETH-PERPETUAL', 'index_price': 1474.68, 'fee_currency': 'ETH', 'fee': 0.0, 'direction': 'sell', 'api': True, 'amount': 78.0}]
         
-        sqlite_management.insert_table_mytrades('myTradesOpen',params)
+        sqlite_management.insert_tables('myTradesOpen',params)
+        
+        params= [{
+            "web": False,
+            "triggered": False,
+            "trigger_price": 1720.0,
+            "trigger": "last_price",
+            "time_in_force": "good_til_cancelled",
+            "stop_price": 1720.0,
+            "risk_reducing": False,
+            "replaced": False,
+            "reduce_only": False,
+            "profit_loss": 0.0,
+            "price": "market_price",
+            "post_only": False,
+            "order_type": "stop_market",
+            "order_state": "untriggered",
+            "order_id": "ETH-SLTB-5655271",
+            "mmp": False,
+            "max_show": 9.0,
+            "last_update_timestamp": 1677934800237,
+            "label": "supplyDemandShort60-closed-1677934800137",
+            "is_liquidation": False,
+            "instrument_name": "ETH-PERPETUAL",
+            "direction": "buy",
+            "creation_timestamp": 1677934800237,
+            "api": True,
+            "amount": 9.0,
+        }, {
+            "web": False,
+            "triggered": False,
+            "trigger_price": 1600.0,
+            "trigger": "last_price",
+            "time_in_force": "good_til_cancelled",
+            "stop_price": 1600.0,
+            "risk_reducing": False,
+            "replaced": False,
+            "reject_post_only": False,
+            "reduce_only": False,
+            "profit_loss": 0.0,
+            "price": 1610.0,
+            "post_only": True,
+            "order_type": "take_limit",
+            "order_state": "untriggered",
+            "order_id": "ETH-TPTS-5655237",
+            "mmp": False,
+            "max_show": 5.0,
+            "last_update_timestamp": 1677903684561,
+            "label": "supplyDemandLong60B-closed-1677903684425",
+            "is_liquidation": False,
+            "instrument_name": "ETH-PERPETUAL",
+            "direction": "sell",
+            "creation_timestamp": 1677903684561,
+            "api": True,
+            "amount": 5.0,
+        }]
+        
+        sqlite_management.insert_tables('ordersOpen',params)
         
         query=sqlite_management.querying_table('myTradesOpen')
+        print (query)
+        query=sqlite_management.querying_table('ordersOpen')
         print (query)
         
         query=sqlite_management.querying_table('myTradesOpen', 'state', '=', 'filled')
