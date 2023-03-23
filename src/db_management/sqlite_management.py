1b4e987687383093ee838bab9de3d5af19f6a266
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-23 12:18:04 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -3,22 +3,33 @@
 import sqlite3
 from contextlib import contextmanager
 
-
 def catch_error(error, idle: int = None) -> list:
     """ """
     from utilities import system_tools
 
     system_tools.catch_error_message(error, idle)
 
-
 def telegram_bot_sendtext(bot_message: str, purpose: str) -> None:
-    from utils import telegram_app
+    from utilities import telegram_app
 
     return telegram_app.telegram_bot_sendtext(bot_message, purpose)
 
+def create_dataBase_sqlite(db_name: str = "src/databases/trading.sqlite3") -> None:
+    """
+    """
+    
+    try:
+        conn = sqlite3.connect(db_name)
+        cur = conn.cursor()
+        conn.commit()
+        conn.close()
+    
+    except Exception as error:
+        print (error)
+    return cur
 
 @contextmanager
-def db_ops(db_name: str = "trading.db") -> None:
+def db_ops(db_name: str = "src/databases/trading.sqlite3") -> None:
     """
     # prepare sqlite initial connection + close
             Return and rtype: None
@@ -42,31 +53,78 @@ def db_ops(db_name: str = "trading.db") -> None:
     else:
         conn.commit()
         conn.close()
+         
+def create_table_mytrades ():
 
+    '''
+    '''   
+    with db_ops() as cur:
+        
+        cur.execute("DROP TABLE IF EXISTS mytrades")
+        
+        # create table name: tickers_futures
+        create_table_mytrades = f'CREATE TABLE IF NOT EXISTS mytrades (   instrument_name TEXT, \
+                                                                            label TEXT, \
+                                                                            direction TEXT, \
+                                                                            amount REAL, \
+                                                                            price REAL, \
+                                                                            state TEXT, \
+                                                                            order_type TEXT, \
+                                                                            timestamp REAL, \
+                                                                            trade_seq REAL, \
+                                                                            trade_id TEXT, \
+                                                                            tick_direction REAL, \
+                                                                            order_id TEXT, \
+                                                                            api BOOLEAN NOT NULL CHECK (api IN (0, 1)))'           
+        try:
+            cur.execute (f'{create_table_mytrades}') 
+        except Exception as error:
+            print(error)
 
-@contextmanager
-def create_dataBase_sqlite(db_name: str = "trading.db") -> None:
-    """
-    # prepare sqlite initial connection + close
-            Return and rtype: None
-            # https://stackoverflow.com/questions/67436362/decorator-for-sqlite3/67436763#67436763
-            # https://charlesleifer.com/blog/going-fast-with-sqlite-and-python/
-    """
+def insert_table_mytrades (params):
 
-    conn = sqlite3.connect(db_name)
-    conn.execute("BEGIN")
-    cur = conn.cursor()
-    yield cur
-    conn.commit()
-    conn.close()
+    '''
+    '''   
+    instrument_name = params ['instrument_name']
+    label = params ['label']
+    direction = params ['direction']
+    amount = params ['amount']
+    price = params ['price']
+    state = params ['state']
+    order_type = params ['order_type']
+    timestamp = params ['timestamp']
+    trade_seq = params ['trade_seq']
+    trade_id = params ['trade_id']
+    tick_direction = params ['tick_direction']
+    order_id = params ['order_id']
+    api = params ['api']
+    #print (params)
+        
+    with db_ops() as cur:
+        
+        insert_table_mytrades= f'INSERT INTO mytrades (instrument_name,  label, direction, amount, price, state, order_type, timestamp, trade_seq, trade_id, tick_direction, order_id, api) VALUES (:instrument_name,  :label, :direction, :amount, :price, :state, :order_type, :timestamp, :trade_seq, :trade_id, :tick_direction, :order_id, :api);'  
 
+        
+        cur.executemany (f'{insert_table_mytrades}', [params])
+        
+                  
+def querying_table_mytrades (table: str = 'mytrades')->list:
 
-if __name__ == "__main__":
+    '''
+            Reference
+            # https://stackoverflow.com/questions/65934371/return-data-from-sqlite-with-headers-python3
+    ''' 
+    query_table = f'SELECT  * FROM {table}' 
+    
     try:
-        create_dataBase_sqlite()
-
-    except (KeyboardInterrupt, SystemExit):
-        catch_error(KeyboardInterrupt)
-
+        with db_ops() as cur:
+            result = list(cur.execute((f'{query_table}')))
+            headers = list(map(lambda attr : attr[0], cur.description))
+                        
+            combine_result = []
+            for i in result:
+                combine_result.append(dict(zip(headers,i)))
+                
     except Exception as error:
-        catch_error(error, 10)
+        print (error)
+
