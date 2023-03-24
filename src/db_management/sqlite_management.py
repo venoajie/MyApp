37435a2466197dc175967014ac3bcea975eddfbf
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 11:56:14 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -168,7 +168,7 @@ async def querying_table (table: str = 'mytrades', filter: str = None, operator=
                     headers = list(head) 
                     
             if filter != None:
-                query_table = f"{query_table}, {filter_value}"
+                query_table = (f'{query_table}, {filter_value}')
                 log.warning(query_table)
                 
                 async with db.execute(query_table) as cur:
