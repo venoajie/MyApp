872888a2cdcb2fdd624fcd0e514653db751564ef
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 08:32:29 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -2,7 +2,7 @@
 
 import sqlite3
 from contextlib import contextmanager
-import asyncio
+import asyncio, aiosqlite
 from loguru import logger as log
 
 def catch_error(error, idle: int = None) -> list:
