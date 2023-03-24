3745594732d870b39c01040c342f2410a99acfb3
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 18:59:46 +0700

    Update fetch_and_save_market_data.py

--- src/fetch_and_save_market_data.py
+++ src/fetch_and_save_market_data.py
@@ -5,8 +5,6 @@
 from pathlib import Path
 
 # installed
-from rocketry import Rocketry
-from rocketry.conds import every
 import requests
 
 # user defined formula
@@ -20,13 +18,6 @@ import time
 symbol = "ETH-PERPETUAL"
 currency = "ETH"
 market_data = get_market_data.MarketData(currency, symbol)
-
-app = Rocketry(
-    config={"task_execution": "async", "restarting": "relaunch", "cycle_sleep": 1}
-)
-
-root = Path(".")
-
 
 def catch_error(error, idle: int = None) -> list:
     """ """
@@ -48,17 +39,18 @@ def get_instruments(currency) -> float:
     )
     return requests.get(endpoint).json()["result"]
 
-
-@app.task(every("3600 seconds"))
 def check_and_save_every_60_minutes():
     try:
         currencies = get_currencies()
         currencies = ["ETH", "BTC"]
         for currency in currencies:
+            
             instruments = get_instruments(currency)
+            
             my_path_instruments = system_tools.provide_path_for_file(
                 "instruments", currency
             )
+            
             pickling.replace_data(my_path_instruments, instruments)
 
         my_path_cur = system_tools.provide_path_for_file("currencies")
@@ -67,70 +59,15 @@ def check_and_save_every_60_minutes():
     except Exception as error:
         catch_error(error)
 
-
-# @app.task(every("5 seconds"))
-def check_and_save_every_30_seconds():
-    try:
-        from synchronizing_files import main
-        import asyncio
-
-        print("AAAAAAA")
-        asyncio.get_event_loop().run_until_complete(main())
-        print("c")
-
-    except Exception as error:
-        catch_error(error)
-
-
-@app.task(every("300 seconds"))
-def check_and_save_every_5_minutes():
-    try:
-        # https://towardsdatascience.com/understand-async-await-with-asyncio-for-asynchronous-programming-in-python-e0bc4d25808e
-        open_interest_historical = market_data.open_interest_historical()
-        my_path = system_tools.provide_path_for_file(
-            "openInterestHistorical", currency.lower()
-        )
-        print (open_interest_historical)
-        pickling.replace_data(my_path, open_interest_historical)
-
-        open_interest_symbol = market_data.open_interest_symbol()
-        file_name = f"{currency.lower()}-openInterestSymbol.pkl"
-        my_path = system_tools.provide_path_for_file(
-            "openInterestHistorical", currency.lower()
-        )
-        pickling.replace_data(my_path, open_interest_symbol)
-
-        open_interest_aggregated_ohlc = market_data.open_interest_aggregated_ohlc()
-        my_path = system_tools.provide_path_for_file(
-            "openInterestAggregated", currency.lower()
-        )
-        pickling.replace_data(my_path, open_interest_aggregated_ohlc)
-
-    except Exception as error:
-        catch_error(error)
-
-
-async def job(message='stuff', n=1):
-    resolution = "m5"
-    connection_url: str = "https://open-api.coinglass.com/public/v2/"
-
-    open_interest_historical = await get_dbt.get_open_interest_aggregated_ohlc(
-        connection_url, "eth-perpetual", resolution
-    )
-    print (open_interest_historical)
-
-
 if __name__ == "__main__":
     
-        
-    for i in range(1,3):
-        schedule.every(1).seconds.do(job, n=i)
-    schedule.every(5).to(10).days.do(job)
-    schedule.every().hour.do(job, message='things')
-    schedule.every().day.at("16:43").do(job)
+    schedule.every().hour.do(check_and_save_every_60_minutes, message='things')
+    schedule.every().day.at("08:01").do(check_and_save_every_60_minutes)
+    schedule.every().day.at("12:01").do(check_and_save_every_60_minutes)
 
     loop = asyncio.get_event_loop()
+    
     while True:
         loop.run_until_complete(schedule.run_pending())
-        time.sleep(0.1)
+        time.sleep(.91)
         
