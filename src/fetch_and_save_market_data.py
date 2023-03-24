8c1d852c23edf379777f23704effb979c7950d17
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 19:53:11 +0700

    Update fetch_and_save_market_data.py

--- src/fetch_and_save_market_data.py
+++ src/fetch_and_save_market_data.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     connection_url: str = "https://www.deribit.com/api/v2/"
     
     #schedule.every().hour.do(check_and_save_every_60_minutes, message='things')
-    schedule.every().day.at("12:53").do(get_instruments, connection_url, currency)
+    schedule.every().day.at("12:54").do(get_instruments, connection_url, currency)
     #schedule.every().day.at("12:02").do(check_and_save_every_60_minutes)
 
     loop = asyncio.get_event_loop()
