cc3033452d467029f9945de2d6350666f9a0a7ee
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 14:32:29 +0700

    Update deribit_get.py

--- src/deribit_get.py
+++ src/deribit_get.py
@@ -53,8 +53,8 @@ async def telegram_bot_sendtext(
             ]
     connection_url = "https://api.telegram.org/bot"
     print(bot_token)
-    print(bot_chatID)
     print(purpose)
+    print(bot_chatID)
     endpoint = (
         bot_token
         + ("/sendMessage?chat_id=")
