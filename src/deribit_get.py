26024445d7b60adcca1b2442d0a37f8c4146d10c
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 17:24:38 +0700

    Update deribit_get.py

--- src/deribit_get.py
+++ src/deribit_get.py
@@ -86,6 +86,10 @@ async def main(
         "method": f"{endpoint}",
         "params": params,
     }
+    print ('coinglass' in endpoint)
+    print (client_id != None or 'coinglass' in endpoint)
+    print (params)
+    print (endpoint)
 
     if client_id != None or 'coinglass' in endpoint:
         async with aiohttp.ClientSession() as session:
