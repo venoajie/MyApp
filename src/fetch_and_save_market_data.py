63ea8f72cdeb3b637b94251c54817e8f72231125
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 20:55:41 +0700

    Update fetch_and_save_market_data.py

--- src/fetch_and_save_market_data.py
+++ src/fetch_and_save_market_data.py
@@ -72,8 +72,7 @@ async def main() -> None:
     currency = 'USD'
     url = f" https://open-api.coinglass.com/public/v2/open_interest_history?symbol={symbol}&time_type=all&currency={currency}"
 
-
-    async with session.get(url=url, headers=headers) as resp:
+    async with session.get(url, headers=headers) as resp:
         print(await resp.text())
         
 if __name__ == "__main__":
