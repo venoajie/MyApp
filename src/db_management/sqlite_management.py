ab16a0f43baf686939b0cb65d9cf8190d8081675
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-24 11:51:54 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -170,7 +170,7 @@ async def querying_table (table: str = 'mytrades', filter: str = None, operator=
             if filter != None:
                 query_table = f'SELECT  * FROM {table} WHERE  state  = ?' 
                 
-                async with db.execute(query_table, {filter_value}) as cur:
+                async with db.execute(f"{query_table}, {filter_value}") as cur:
                     fetchall =  (await cur.fetchall())
                 
                     head = (map(lambda attr : attr[0], cur.description))
