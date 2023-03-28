8f764fbb689ac4c5109217c2e69b96a29f196a91
venoajie <59521823+venoajie@users.noreply.github.com>
2023-03-28 21:31:22 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -184,7 +184,7 @@ async def insert_tables (table_name, params):
             else:
             
                 if 'json' in table_name:
-                    insert_table_json= f'INSERT INTO {table_name} VALUES json ([{params}]);'
+                    insert_table_json= f'''INSERT INTO {table_name} (data) VALUES (json('{params}'));'''
                     print (insert_table_json)
                     await db.execute (insert_table_json)
                 else:
