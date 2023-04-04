33f8b1d4c64509da72d8ae85a356396d8d6d76f2
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-05 06:08:21 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -345,11 +345,11 @@ class ApplyHedgingSpot:
 
         # formatting label: strategy & int. Result example: 'hedgingSpot'/'supplyDemandShort60'
         strategy_label = str_mod.get_strings_before_character(label, "-", 0)
-        # log.warning(f'strategy_label {strategy_label}')
+        log.warning(f'strategy_label {strategy_label}')
         my_trades_open_sqlite: list = await self.querying_all('my_trades_all_json')
-        sum_my_trades_open_sqlite: list = [
+        sum_my_trades_open_sqlite: list = sum([
             o['amount_dir'] for o in my_trades_open_sqlite if  str_mod.get_strings_before_character(o['label_main'], "-", 0) == strategy_label
-        ]
+        ])
         log.error (sum_my_trades_open_sqlite)
         
 
