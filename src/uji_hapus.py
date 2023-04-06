ee4d93141ea6194164b7b52575b54962ef3fcc74
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-06 14:24:09 +0700

    Update uji_hapus.py

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -5,11 +5,9 @@ import os
 import sqlite3
 import time
 import ast
-#from db_management import sqlite_management
+from db_management import sqlite_management
 #standardize database file location
 import json, orjson
-
-
 
 path = os.getcwd()
 db_name = 'multistrike_oi.db'
@@ -20,31 +18,13 @@ my_trades_open =  [{'trade_seq': 119459281, 'trade_id': 'ETH-162634254', 'timest
 
 my_trades_path = system_tools.provide_path_for_file("myTrades", "eth", "open")
 
-
-
 my_trades_path_open_recovery = system_tools.provide_path_for_file(
     "myTrades", "eth", "open-recovery-point"
 )
 my_trades  = pickling.read_data(my_trades_path_open_recovery)
 
-
 net = number_modification.net_position(my_trades_open)
 print (net)
-
-test = [{'id': 1, 'data': '{"trade_seq":119459281,"trade_id":"ETH-162634254","timestamp":1678610180143,"tick_direction":0,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1473.05,"post_only":true,"order_type":"limit","order_id":"ETH-32205761779","mmp":false,"matching_id":null,"mark_price":1472.79,"liquidity":"M","label":"hedgingSpot-open-1678610144572","instrument_name":"ETH-PERPETUAL","index_price":1474.68,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":78.0}', 'label_main': 'hedgingSpot-open-1678610144572', 'amount_dir': -78.0}, {'id': 2, 'data': '{"trade_seq":119653801,"trade_id":"ETH-162909430","timestamp":1678706775993,"tick_direction":1,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1583.75,"post_only":true,"order_type":"limit","order_id":"ETH-32231983208","mmp":false,"matching_id":null,"mark_price":1583.81,"liquidity":"M","label":"hedgingSpot-open-1678706775367","instrument_name":"ETH-PERPETUAL","index_price":1583.99,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":5.0}', 'label_main': 'hedgingSpot-open-1678706775367', 'amount_dir': -5.0}, {'id': 3, 'data': '{"trade_seq":119665688,"trade_id":"ETH-162925875","timestamp":1678713118256,"tick_direction":0,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1598.0,"post_only":true,"order_type":"limit","order_id":"ETH-32234303212","mmp":false,"matching_id":null,"mark_price":1597.73,"liquidity":"M","label":"hedgingSpot-open-1678713106585","instrument_name":"ETH-PERPETUAL","index_price":1597.86,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":1.0}', 'label_main': 'hedgingSpot-open-1678713106585', 'amount_dir': -1.0}, {'id': 4, 'data': '{"trade_seq":119736857,"trade_id":"ETH-163020718","timestamp":1678727097665,"tick_direction":0,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1666.15,"post_only":true,"order_type":"limit","order_id":"ETH-32243915623","mmp":false,"matching_id":null,"mark_price":1666.07,"liquidity":"M","label":"hedgingSpot-open-1678727089260","instrument_name":"ETH-PERPETUAL","index_price":1666.08,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":4.0}', 'label_main': 'hedgingSpot-open-1678727089260', 'amount_dir': -4.0}, {'id': 5, 'data': '{"trade_seq":119736858,"trade_id":"ETH-163020719","timestamp":1678727097665,"tick_direction":1,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1666.15,"post_only":true,"order_type":"limit","order_id":"ETH-32243915636","mmp":false,"matching_id":null,"mark_price":1666.07,"liquidity":"M","label":"hedgingSpot-open-1678727089307","instrument_name":"ETH-PERPETUAL","index_price":1666.08,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":4.0}', 'label_main': 'hedgingSpot-open-1678727089307', 'amount_dir': -4.0}, {'id': 6, 'data': '{"trade_seq":119736859,"trade_id":"ETH-163020720","timestamp":1678727097665,"tick_direction":1,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1666.15,"post_only":true,"order_type":"limit","order_id":"ETH-32243915644","mmp":false,"matching_id":null,"mark_price":1666.07,"liquidity":"M","label":"hedgingSpot-open-1678727089354","instrument_name":"ETH-PERPETUAL","index_price":1666.08,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":4.0}', 'label_main': 'hedgingSpot-open-1678727089354', 'amount_dir': -4.0}, {'id': 7, 'data': '{"trade_seq":120460950,"trade_id":"ETH-163959270","timestamp":1679205009236,"tick_direction":1,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1783.95,"post_only":true,"order_type":"limit","order_id":"ETH-32404150932","mmp":false,"matching_id":null,"mark_price":1784.05,"liquidity":"M","label":"hedgingSpot-open-1679204971613","instrument_name":"ETH-PERPETUAL","index_price":1782.8,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":7.0}', 'label_main': 'hedgingSpot-open-1679204971613', 'amount_dir': -7.0}, {'id': 8, 'data': '{"trade_seq":120460951,"trade_id":"ETH-163959271","timestamp":1679205009236,"tick_direction":1,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1783.95,"post_only":true,"order_type":"limit","order_id":"ETH-32404150944","mmp":false,"matching_id":null,"mark_price":1784.05,"liquidity":"M","label":"hedgingSpot-open-1679204971663","instrument_name":"ETH-PERPETUAL","index_price":1782.8,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":7.0}', 'label_main': 'hedgingSpot-open-1679204971663', 'amount_dir': -7.0}, {'id': 9, 'data': '{"trade_seq":120460952,"trade_id":"ETH-163959272","timestamp":1679205009236,"tick_direction":1,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1783.95,"post_only":true,"order_type":"limit","order_id":"ETH-32404150950","mmp":false,"matching_id":null,"mark_price":1784.05,"liquidity":"M","label":"hedgingSpot-open-1679204971720","instrument_name":"ETH-PERPETUAL","index_price":1782.8,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":7.0}', 'label_main': 'hedgingSpot-open-1679204971720', 'amount_dir': -7.0}, {'id': 10, 'data': '{"trade_seq":120473261,"trade_id":"ETH-163975417","timestamp":1679218646776,"tick_direction":1,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1780.65,"post_only":true,"order_type":"limit","order_id":"ETH-32406588944","mmp":false,"matching_id":null,"mark_price":1780.49,"liquidity":"M","label":"hedgingSpot-open-1679218645798","instrument_name":"ETH-PERPETUAL","index_price":1779.95,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":7.0}', 'label_main': 'hedgingSpot-open-1679218645798', 'amount_dir': -7.0}, {'id': 11, 'data': '{"trade_seq":120460946,"trade_id":"ETH-163959266","timestamp":1679205009236,"tick_direction":1,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1783.95,"post_only":true,"order_type":"limit","order_id":"ETH-32404150901","mmp":false,"matching_id":null,"mark_price":1784.05,"liquidity":"M","label":"hedgingSpot-open-1679204971425","instrument_name":"ETH-PERPETUAL","index_price":1782.8,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":7.0}', 'label_main': 'hedgingSpot-open-1679204971425', 'amount_dir': -7.0}, {'id': 12, 'data': '{"trade_seq":120460945,"trade_id":"ETH-163959265","timestamp":1679205009236,"tick_direction":0,"state":"filled","self_trade":false,"risk_reducing":false,"reduce_only":false,"profit_loss":0.0,"price":1783.95,"post_only":true,"order_type":"limit","order_id":"ETH-32404150899","mmp":false,"matching_id":null,"mark_price":1784.05,"liquidity":"M","label":"hedgingSpot-open-1679204971378","instrument_name":"ETH-PERPETUAL","index_price":1782.8,"fee_currency":"ETH","fee":0.0,"direction":"sell","api":true,"amount":5.0}', 'label_main': 'hedgingSpot-open-1679204971378', 'amount_dir': -5.0}]
-#print (test)
-json_load = (json.loads(json.dumps ( (test))))
-#print ([ast.literal_eval(i) for i in json_load])
-
-json_load =   (
-    ([o ['data']for o in json_load 
-        ]
-       )
-    )
-
-json_ld =  string_modification.parsing_sqlite_json_output (json_load)
-print (json_ld)
-
 
 
 async  def main() -> list:
@@ -54,9 +34,6 @@ async  def main() -> list:
     await sqlite_management.insert_tables ('my_trades_all',
                                  my_trades_open)
 
-
-    #query=await sqlite_management.querying_table('myTradesOpen', 'state', '=', 'filled')
-    #print (query)
     
 if __name__ == "__main__":
         
