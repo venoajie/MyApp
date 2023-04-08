b644bf9a24424aad410c6f73b3ac2d376285a961
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-08 12:18:59 +0700

    Update capture_exch_data_fr_deribit.py

--- src/capture_exch_data_fr_deribit.py
+++ src/capture_exch_data_fr_deribit.py
@@ -233,7 +233,7 @@ class StreamAccountData:
                                                 
                                         order_id = order["order_id"] if order_state !='triggered' else ["stop_order_id'"]
                                         log.error (f'order_id {order_id}')
-                                        open_orders_sqlite =  await syn.querying_all('my_trades_all_json')
+                                        open_orders_sqlite =  await syn.querying_all('orders_all_json')
                                         open_orders_sqlite_list_data =  open_orders_sqlite['list_data_only']
                                         log.error (f'open_orders_sqlite {open_orders_sqlite_list_data}')
                                         is_order_id_in_active_orders = ([o for o in open_orders_sqlite_list_data if o['order_id']== order_id])
