546b037eb33728f607e4924c7b4d8df5ed1d2359
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-14 01:52:03 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -381,8 +381,9 @@ class ApplyHedgingSpot:
                                                         "=",
                                                         res
                                                         )
-                    log.debug ([o for o in result_transactions if o['trade_seq'] == res])
-                    await sqlite_management.insert_tables('my_trades_closed_json',[o for o in result_transactions if o['trade_seq'] == res])
+                    result_to_dict = ([o for o in result_transactions if o['trade_seq'] == res])[0]['data']
+                    log.error (f' result_to_dict {result_to_dict}')
+                    await sqlite_management.insert_tables('my_trades_closed_json',result_to_dict)
 
     async def deleting_cancel_order(self, table: list, 
                            database: str ,
