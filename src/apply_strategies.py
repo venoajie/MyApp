93276eb23e8b9cf104c0b8929e4392499c6f3e2a
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-13 14:16:42 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -351,14 +351,14 @@ class ApplyHedgingSpot:
         
         trades_with_closed_labels = [o for o in transactions if 'closed' in o['label_main'] ]
         for transactions in trades_with_closed_labels:
+            log.error (transactions)
             label = str_mod.remove_redundant_elements(
                     [
-                        str_mod.get_strings_before_character(o["label"])
+                        str_mod.get_strings_before_character(o["label_main"])
                         for o in [transactions]
                     ]
                 )
             log.error (label)
-            log.error (transactions)
             
             result_transactions = [] if transactions==[] else ([
                 o for o in await self.my_trades_open_sqlite_detailing ([transactions], label, detail_level) ])
