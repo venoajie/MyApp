feeb4224b84c49c2103a881e49a68ce6c6afab11
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-15 21:14:16 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -886,7 +886,6 @@ class ApplyHedgingSpot:
                             ]  
                             
                             log.critical(f" strategy_label  {strategy_label} open_trade_strategy  {open_trade_strategy}")    
-                            log.critical("every" in strategy_attr["strategy"])    
                                                     
                             if "every" in strategy_attr["strategy"]:
                                 
