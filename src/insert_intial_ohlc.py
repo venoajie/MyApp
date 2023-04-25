1049bfb7f488c44e52a3d289fd07f0e2a8f02be4
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-25 18:17:47 +0700

    Create insert_intial_ohlc.py

---
+++ src/insert_intial_ohlc.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# built ins
+from datetime import datetime, timedelta
+import json
+
+# installed
+import asyncio
+from loguru import logger as log
+
+# user defined formula
+from utilities import system_tools, string_modification
+from db_management import sqlite_management
+
+async def telegram_bot_sendtext(bot_message, purpose: str = "general_error") -> None:
+    import deribit_get
+
+    return await deribit_get.telegram_bot_sendtext(bot_message, purpose)
+
+
+async def insert_ohlc(instrument_name: str='ETH-PERPETUAL', resolution: int =1, qty_candles: int =6000) -> None:
+
+    from utilities import time_modification
+    import requests
+
+    now_utc = datetime.now()
+    now_unix = time_modification.convert_time_to_unix (now_utc)
+    start_timestamp = now_unix - 60000 * qty_candles
+    
+    ohlc_endPoint=  (f' https://deribit.com/api/v2/public/get_tradingview_chart_data?end_timestamp={now_unix}&instrument_name={instrument_name}&resolution={resolution}&start_timestamp={start_timestamp}')
+    
+    try:            
+
+        ohlc_request= requests.get(ohlc_endPoint).json()['result']
+        result= string_modification.transform_nested_dict_to_list(ohlc_request)
+        log.warning (result)
+        
+        for data in result:
+            log.error (data)
+            await sqlite_management.insert_tables('ohlc1_eth_perp_json',data)
+
+    except Exception as error:
+        system_tools.catch_error_message(error, 10,"WebSocket connection - failed to get ohlc",)
+    
+
+def main():
+    try:
+        insert_ohlc()
+
+    except Exception as error:
+        system_tools.catch_error_message(
+            error, 10, "fetch and save MARKET data from deribit"
+        )
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except (KeyboardInterrupt, SystemExit):
+        asyncio.get_event_loop().run_until_complete(main().stop_ws())
+
+    except Exception as error:
+        system_tools.catch_error_message(
+            error, 10, "fetch and save MARKET data from deribit"
+        )
