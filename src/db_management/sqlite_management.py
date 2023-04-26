d36aa3a184748a0043949229f5207e4b02b35b5a
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-26 08:04:26 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -90,6 +90,7 @@ async def create_tables (type:str = None):
                  'ohlc3_eth_perp_json',
                  'ohlc5_eth_perp_json',
                  'ohlc15_eth_perp_json',
+                 'ohlc30_eth_perp_json',
                  'ohlc1H_eth_perp_json',
                  'positions_json',
                  'portfolio_json',
