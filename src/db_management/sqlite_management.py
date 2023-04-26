7fee20bfb8eaea2d3da139ea2df06d74fc630c91
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-26 13:17:21 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -533,7 +533,7 @@ def query_pd (table_name: str, field: str = None):
     
     #transform dataframe to dict
     result = result.to_dict('records')
-    result = str_mod.parsing_sqlite_json_output(result)
+    result = str_mod.parsing_sqlite_json_output([result])
 
     pd.json_normalize(result, record_path=['data']) 
     print('AAAAAAAAAAAAAAAAAAAAAAAA')      
