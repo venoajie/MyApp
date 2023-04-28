895ad672b2d2720f84524b531224a8f97cece97c
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-29 05:13:59 +0700

    Update market_profile.py

--- src/market_profile.py
+++ src/market_profile.py
@@ -8,17 +8,6 @@ import numpy as np
 from loguru import logger as log
 from db_management import sqlite_management
 
-#import dash
-#import dash_core_components as dcc
-#import dash_html_components as html
-#from dash.dependencies import Input, Output
-#import plotly.graph_objs as go
-#import warnings
-#warnings.filterwarnings('ignore')
-
-#app = dash.Dash(__name__)
-pd.set_option('display.max_rows', None)
-
 async def querying_all(table: list, 
                         database: str = "databases/trading.sqlite3") -> dict:
     """ """
@@ -48,6 +37,7 @@ def transform_result_to_data_frame (data: object):
     df['volume']= df['volume'].round(decimals = 2)
     
     return df   
+
 def get_ticksize(data, freq=30):
     # data = dflive30
     numlen = int(len(data) / 2)
@@ -101,92 +91,9 @@ for d in range(0, len(dfnflist)):
 date_time_close = dt.datetime.today().strftime('%Y-%m-%d') + ' ' + '23:59:59'
 append_dt = pd.Timestamp(date_time_close)
 dates.append(append_dt)
-#date_mark = {str(h): {'label': str(h), 'style': {'color': 'blue', 'fontsize': '4',
-#                                                 'text-orientation': 'upright'}} for h in range(0, len(dates))}
 
 mp = MpFunctions(data=df.copy(), freq=freq, style=mode, avglen=avglen, ticksize=ticksz, session_hr=trading_hr)
 mplist = mp.get_context()
-
-#app.layout = html.Div(
-#    html.Div([
-#        dcc.Location(id='url', refresh=False),
-#        html.Br(),
-#        html.H4('@beinghorizontal'),
-#        dcc.Graph(id='beinghorizontal'),
-#        dcc.Interval(
-#            id='interval-component',
-#            interval=5 * 1000,  # Reduce the time if you want frequent updates 5000 = 5 sec
-#            n_intervals=0
-#        ),
-#       html.P([
-#            html.Label("Time Period"),
-#            dcc.RangeSlider(id='slider',
-#                            pushable=1,
-#                            marks=date_mark,
-#                            min=0,
-#                            max=len(dates),
-#                            step=None,
-#                            value=[len(dates) - 2, len(dates) - 1])
-#        ], style={'width': '80%',
-#                  'fontSize': '14px',
-#                  'padding-left': '100px',
-#                  'display': 'inline-block'})
-#    ])
-#)
-
-
-#@app.callback(Output(component_id='beinghorizontal', component_property='figure'),
-#              [Input('interval-component', 'n_intervals'),
-#               Input('slider', 'value')
-#               ])
-def update_graph(n, value):
-
-    distribution_hist = mplist[1]
-
-    url_1m = "https://www.binance.com/api/v1/klines?symbol=ETHBUSD&interval=1m"
-
-    df_live1 = get_data(url_1m)  # this line fetches new data for current day
-    df_live1 = df_live1.dropna()
-
-    dflive30 = df_live1.resample('30min').agg({'datetime': 'last', 'Open': 'first', 'High': 'max', 'Low': 'min',
-                                               'Close': 'last', 'volume': 'sum'})
-    df2 = pd.concat([df, dflive30])
-    df2 = df2.drop_duplicates('datetime')
-
-    ticksz_live = (get_ticksize(dflive30.copy(), freq=2))
-    mplive = MpFunctions(data=dflive30.copy(), freq=freq, style=mode, avglen=avglen, ticksize=ticksz_live,
-                         session_hr=trading_hr)
-
-    mplist_live = mplive.get_context()
-
-    df_distribution_live = mplist_live[1]
-    df_distribution_concat = pd.concat([distribution_hist, df_distribution_live], axis=0)
-    df_distribution_concat = df_distribution_concat.reset_index(inplace=False, drop=True)
-
-    df_updated_rank = mp.get_dayrank()
-    ranking = df_updated_rank[0]
-    value=[len(dates) - 2, len(dates) - 1]
-    
-    for inc in range(value[1] - value[0]):
-        i = value[0]
-        
-        i += inc
-        
-        irank = ranking.iloc[i]  # select single row from ranking df
-
-        log.debug (f' irank {irank}')
-        
-        #dict_result=dict (volume= list_example['volume'][k],
-        #               tick= list_example['ticks'][k],
-        #               open= list_example['open'][k],
-        #               low= list_example['low'][k],
-        #               high= list_example['high'][k],
-        #               cost= list_example['cost'][k],
-        #               close= list_example['close'][k])
-        
-        #my_list.append(dict_result)
-        
-    return 
 
 def get_market_profile():
 
