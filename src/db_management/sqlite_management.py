3890efec694fbc3928809d913358dae3054c7735
venoajie <59521823+venoajie@users.noreply.github.com>
2023-04-29 16:20:43 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -592,7 +592,7 @@ async def replace_row (new_value: dict, column_name: str='data', table: str = 'o
                     
     try:
         #UPDATE ohlc1_eth_perp_json SET data = json_replace('{"volume":36.475044,"tick":1682755380000,"open":1905.7,"low":1905.55,"high":1905.7,"cost":69506.0,#"close":1905.55}') WHERE id is 9542;
-        query_table = f"""UPDATE {table} SET {column_name} = json_replace('{json.dumps(new_value)}'  WHERE  JSON_EXTRACT (data, '$.tick') {operator} {filter_value};"""
+        query_table = f"""UPDATE {table} SET {column_name} = json_replace('{json.dumps(new_value)}'  WHERE  (data, '$.tick') {operator} {filter_value};"""
         print (f'query_table {query_table}')
 
         async with  aiosqlite.connect(database, isolation_level=None) as db:
