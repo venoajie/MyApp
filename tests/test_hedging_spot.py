9165f1a96bc8b306652b3f344b124340c41518dd
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-03 04:45:01 +0700

    Update test_hedging_spot.py

--- tests/test_hedging_spot.py
+++ tests/test_hedging_spot.py
@@ -4,12 +4,36 @@ import strategies.entries_exits as entry
 
 strategy_attributes = entry.strategies
 
+strategy_attributes_for_hedging=  [o for o in strategy_attributes if 'hedgingSpot' in o ['strategy']  ]
+
+notional = 100
+
+ask_price= 1900
+
 def test_get_basic_opening_paramaters():
+        
+    result =  hedging.get_basic_opening_paramaters(notional, ask_price)
     
-    notional = 100
+    assert result   ==  {'type': 'limit', 'side': 'sell', 'size': 10, 'type': 'limit', 'entry_price': 1900}
+
+def test_are_size_and_order_appropriate_for_ordering():
     
-    strategy_attributes_for_hedging=  [o for o in strategy_attributes if 'hedgingSpot' in o ['strategy']  ]
+    current_size= 20
+    current_outstanding_order_len= 0
+    result =  hedging.are_size_and_order_appropriate_for_ordering(notional, 
+                                                                  current_size, 
+                                                                  current_outstanding_order_len)
+    assert result   == True
     
-    result =  hedging.get_basic_opening_paramaters(notional, strategy_attributes_for_hedging)
-    
-    assert result   ==  {'label_numbered': 'limit', 'side': 'sell', 'size': 10, 'type': 'limit'}
+    current_size= 101
+    result =  hedging.are_size_and_order_appropriate_for_ordering(notional, 
+                                                                  current_size, 
+                                                                  current_outstanding_order_len)
+    assert result   == False
+
+    current_size= 99
+    current_outstanding_order_len= 1
+    result =  hedging.are_size_and_order_appropriate_for_ordering(notional, 
+                                                                  current_size, 
+                                                                  current_outstanding_order_len)    
+    assert result   == False
