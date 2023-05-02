80db32f8b4a7a837ba19dc0eff97a92290c8fc17
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-03 06:39:53 +0700

    Update test_hedging_spot.py

--- tests/test_hedging_spot.py
+++ tests/test_hedging_spot.py
@@ -37,3 +37,23 @@ def test_are_size_and_order_appropriate_for_ordering():
                                                                   current_size, 
                                                                   current_outstanding_order_len)    
     assert result   == False
+
+
+def test_get_labels():
+
+    from src.utilities import time_modification
+    
+    now_utc = time_modification.convert_time_to_utc()["utc_now"]
+    now_unix = time_modification.convert_time_to_unix(now_utc)
+    
+    label_main_or_label_transactions= 'hedgingSpot'
+        
+    result =  hedging.get_label('open', label_main_or_label_transactions)[:25]
+    
+    assert result   ==  f"{label_main_or_label_transactions}-open-{now_unix}"[:25]
+
+    label_main_or_label_transactions= 'hedgingSpot-open-1683065013136'
+    
+    result =  hedging.get_label('closed', label_main_or_label_transactions)
+    
+    assert result   ==  'hedgingSpot-closed-1683065013136'
