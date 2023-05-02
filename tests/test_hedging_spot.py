3a10b883c44d7a19bcd7440b2b167c2a294f4624
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-02 18:17:53 +0700

    Create test_hedging_spot.py

---
+++ tests/test_hedging_spot.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import strategies.hedging_spot as hedging
+import strategies.entries_exits as entry
+
+strategy_attributes = entry.strategies
+
+def test_get_basic_opening_paramaters():
+    
+    notional = 100
+    
+    strategy_attributes_for_hedging=  [o for o in strategy_attributes if 'hedgingSpot' in strategy_attributes  ]
+    
+    result =  hedging.get_basic_opening_paramaters(notional, strategy_attributes_for_hedging)
+    
+    assert result   ==  {'label_numbered': 'limit', 'side': 'sell', 'size': 10, 'type': 'limit'}
