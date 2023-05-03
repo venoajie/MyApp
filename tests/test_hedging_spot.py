458f45dadf89c3ce72a36a2e42ced7c782953c6d
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-03 14:39:52 +0700

    Update test_hedging_spot.py

--- tests/test_hedging_spot.py
+++ tests/test_hedging_spot.py
@@ -18,20 +18,20 @@ def test_get_basic_opening_paramaters():
 
 def test_are_size_and_order_appropriate_for_ordering():
     
-    current_size= 20
+    current_size= -20
     current_outstanding_order_len= 0
     result =  hedging.are_size_and_order_appropriate_for_ordering(notional, 
                                                                   current_size, 
                                                                   current_outstanding_order_len)
     assert result   == True
     
-    current_size= 101
+    current_size= -101
     result =  hedging.are_size_and_order_appropriate_for_ordering(notional, 
                                                                   current_size, 
                                                                   current_outstanding_order_len)
     assert result   == False
 
-    current_size= 99
+    current_size= -99
     current_outstanding_order_len= 1
     result =  hedging.are_size_and_order_appropriate_for_ordering(notional, 
                                                                   current_size, 
