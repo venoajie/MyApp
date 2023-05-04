eae23702fcb4e1a5b977a91e9d9a30890861f01a
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-05 06:57:29 +0700

    Update hedging_spot.py

--- src/strategies/hedging_spot.py
+++ src/strategies/hedging_spot.py
@@ -149,18 +149,14 @@ def is_transaction_price_minus_below_threshold(last_transaction_price: float,
                                                         current_price: float,
                                                         pct_threshold: float
                                                         )-> bool:    
-    print (f'last_transaction_price {last_transaction_price}')
-    print (f'current_price {current_price}')
-    print (f'price_minus_pct {price_minus_pct (last_transaction_price, pct_threshold)}')
+    
     return price_minus_pct (last_transaction_price, pct_threshold) > current_price
 
 def is_transaction_price_plus_above_threshold(last_transaction_price: float,
                                               current_price: float,
                                               pct_threshold: float
-                                              )-> bool:    
-    print (f'last_transaction_price {last_transaction_price}')
-    print (f'current_price {current_price}')
-    print (f'price_plus_pct {price_plus_pct (last_transaction_price, pct_threshold)}')
+                                              )-> bool:  
+    
     return price_plus_pct (last_transaction_price, pct_threshold) < current_price
 
 def is_minimum_waiting_time_has_exceeded(last_transaction_timestamp: int)-> bool:    
