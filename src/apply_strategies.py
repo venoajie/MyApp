fed1c9afa05ba2bbf89c9d35a197a9d4f8c22433
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-09 19:42:19 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -101,7 +101,7 @@ class ApplyHedgingSpot:
             all= [] if result in NONE_DATA \
                 else (result),
             certain_data= [] if result in NONE_DATA \
-                else (([o['label_main', 'amount_dir'] for o in result ])),
+                else (([o(['label_main'], ['amount_dir']) for o in result ])),
             list_data_only= [] if result in NONE_DATA \
                 else str_mod.parsing_sqlite_json_output([o['data'] for o in result])
                     )
