6c99462d64b9afeccb606dac1cbd5302567499ae
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-10 07:02:15 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -283,11 +283,11 @@ class ApplyHedgingSpot:
         """ """
         
         proforma_size=   (current_size + sum_current_open_order + sum_next_open_order)
-        relevant_label= ['hedging' , 'Long']
+        relevant_label= ['hedging' , 'Short']
         relevant_open_trade= [o for o in label_and_size if ([r for r in relevant_label if r in o['label_main']])]
 
         log.error (relevant_open_trade)
-        log.error (label_and_size)
+        #log.error (label_and_size)
         
         return dict(
             position=   (current_size + sum_current_open_order + sum_next_open_order),
