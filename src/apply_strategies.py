84c7be121f2b82093ccff25253a72b45bfcb4b9f
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-11 06:07:18 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -635,10 +635,13 @@ class ApplyHedgingSpot:
                         log.error (send_additional_order)
                         sum_next_open_order= send_additional_order['order_parameters']['size']
                         
-                        proforma_size: int = await balancer.is_send_open_order_allowed (best_ask_prc, best_bid_prc, 
+                        send_open_order_allowed: int = await balancer.is_send_open_order_allowed (best_ask_prc, best_bid_prc, 
                                                                       sum_next_open_order)
+                        send_exit_order_allowed: int = await balancer.is_send_exit_order_allowed (best_ask_prc, best_bid_prc)
                         
-                        log.error (f'proforma_size {proforma_size}')
+                        
+                        log.error (f'send_open_order_allowed {send_open_order_allowed}')
+                        log.error (f'send_exit_order_allowed {send_exit_order_allowed}')
                         
                         await self.if_order_is_true(send_additional_order)
                                           
