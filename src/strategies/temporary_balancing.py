765d7650a2d2217c5c9bd54e0839a97801341b18
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-11 04:39:51 +0700

    Update temporary_balancing.py

--- src/strategies/temporary_balancing.py
+++ src/strategies/temporary_balancing.py
@@ -117,6 +117,6 @@ async def is_send_open_order_allowed (ask_price: float,
         if proforma_size['order_size']<0:
             params.update({"entry_price": bid_price})
             params.update({"side": "buy"})
-    
+    print (params)
     return dict(order_allowed= order_allowed,
                 order_parameters= [] if order_allowed== False else params)
