f2b40c1a5a8d850d949fc72702a4618c8987105e
venoajie <59521823+venoajie@users.noreply.github.com>
2023-05-29 21:54:01 +0700

    Update loss_leadership.py

--- src/risk_management/loss_leadership.py
+++ src/risk_management/loss_leadership.py
@@ -1,1 +1,21 @@
-# https://quant.stackexchange.com/questions/9527/is-price-gaping-the-major-risk-that-market-maker-has
+
+"""
+https://quant.stackexchange.com/questions/9527/is-price-gaping-the-major-risk-that-market-maker-has
+
+""" 
+
+def compute_individual_loss(ask_price: float,
+                            bid_price: float,
+                            side: str,
+                            acquisition_cost: float) -> dict:
+    """
+
+
+    """ 
+    if side== 'sell':
+        loss_in_usd= acquisition_cost - bid_price
+    
+    if side== 'buy':
+        loss_in_usd= ask_price - acquisition_cost
+    
+    return loss_in_usd
