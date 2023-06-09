7b88922db79c262f941698574593c69c3ea9187c
venoajie <59521823+venoajie@users.noreply.github.com>
2023-06-09 18:23:36 +0700

    Update market_maker.py

--- src/strategies/market_maker.py
+++ src/strategies/market_maker.py
@@ -38,9 +38,9 @@ class MarketMaker(BasicStrategy):
 
         """
         strategy_config= self.get_basic_params().get_strategy_config()
-        orders= self.get_basic_params().get_orders_attributes()
+        orders= await self.get_basic_params().get_orders_attributes()
         len_orders= orders['transactions_len']
-        my_trades= self.get_basic_params().get_my_trades_attributes()
+        my_trades= await elf.get_basic_params().get_my_trades_attributes()
         len_my_trades= my_trades['transactions_len']
 
         order_allowed= True
