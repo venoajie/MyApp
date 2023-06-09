7350dd36b53aaa1edf69d4a19232363437f0ff1a
venoajie <59521823+venoajie@users.noreply.github.com>
2023-06-09 16:24:43 +0700

    Update basic_strategy.py

--- src/strategies/basic_strategy.py
+++ src/strategies/basic_strategy.py
@@ -23,6 +23,9 @@ class BasicStrategy:
         from strategies import entries_exits
         
         params= entries_exits.strategies
+        print(f'strategy_label {self.strategy_label}')
+        print(f'params {params}')
+        
         return [o for o in params if self.strategy_label in o["strategy"]]  [0]
 
     def get_basic_opening_paramaters(self, notional: float= None) -> dict:
