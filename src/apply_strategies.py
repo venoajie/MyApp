3ee4b7dc64013c8873e28e62f593f72b5fe39e3a
venoajie <59521823+venoajie@users.noreply.github.com>
2023-06-10 16:55:50 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -348,7 +348,6 @@ class ApplyHedgingSpot:
         1. remove them from db for open transactions/my_trades_all_json
         2. move them to table for closed transactions/my_trades_closed_json
         """
-        log.debug (transactions_all)
         transactions_all= [o['data'] for o in transactions_all]
         log.warning (transactions_all)
                         
