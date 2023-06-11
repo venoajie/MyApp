26148f4c44baafde57c8516367290a46ce676745
venoajie <59521823+venoajie@users.noreply.github.com>
2023-06-11 09:46:50 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -816,12 +816,12 @@ class ApplyHedgingSpot:
                 strategies = entries_exits.strategies
     
                 
-                log.error (my_trades_open)
+                #log.error (my_trades_open)
                 #log.error ([o["label"] for o in my_trades_open])
                 my_trades_open_remove_closed_labels = [] if my_trades_open == [] \
                     else [o for o in my_trades_open if 'closed' not in o["label"]]
                                         
-                log.debug (my_trades_open_remove_closed_labels)
+                #log.debug (my_trades_open_remove_closed_labels)
                 
                 label_transaction_net =  [] if my_trades_open_remove_closed_labels == [] \
                     else str_mod.remove_redundant_elements(
