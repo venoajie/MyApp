7d94d390b52b77007ab6b80c848f382b1e7e66f9
venoajie <59521823+venoajie@users.noreply.github.com>
2023-06-12 09:52:05 +0700

    Update market_maker.py

--- src/strategies/market_maker.py
+++ src/strategies/market_maker.py
@@ -7,7 +7,6 @@ import asyncio
 from dataclassy import dataclass
 
 # user defined formula
-from strategies import hedging_spot
 from strategies.basic_strategy import BasicStrategy 
 
 @dataclass(unsafe_hash=True, slots=True)
@@ -21,81 +20,70 @@ class MarketMaker(BasicStrategy):
         return BasicStrategy(self.strategy_label)
 
     async def is_send_and_cancel_open_order_allowed (self,
-                                          notional: float,
-                                          ask_price: float,
-                                          bid_price: float,
-                                          server_time: int
-                                          ) -> dict:
+                                                     notional: float,
+                                                     ask_price: float,
+                                                     bid_price: float,
+                                                     server_time: int
+                                                     ) -> dict:
         """
-
-        Args:
-
-        Returns:
-            dict
-
-        """
-        strategy_config= self.get_basic_params().get_strategy_config()
+        """        
+        open_orders_label_strategy: dict= await self.get_basic_params().get_orders_attributes('open')
         
-        open_orders_label_strategy= await self.get_basic_params().get_orders_attributes('open')
+        len_orders: int= open_orders_label_strategy['transactions_len']
+        my_trades: dict= await self.get_basic_params().get_my_trades_attributes()
+        len_my_trades: int= my_trades['transactions_len']
+        max_tstamp_my_trades: int= my_trades['max_time_stamp']
         
-        len_orders= open_orders_label_strategy['transactions_len']
-        my_trades= await self.get_basic_params().get_my_trades_attributes()
-        len_my_trades= my_trades['transactions_len']
-        max_tstamp_my_trades= my_trades['max_time_stamp']
+        params: dict= self.get_basic_params().get_basic_opening_paramaters(notional,
+                                                                           ask_price,
+                                                                           bid_price
+                                                                           )
         
-        params= self.get_basic_params().get_basic_opening_paramaters(notional)
-        time_interval= params['interval_time_between_order_in_ms']
+        time_interval: float= params['interval_time_between_order_in_ms']
         
-        order_allowed= False
-        cancel_allowed= False
+        order_allowed: bool= False
+        cancel_allowed: bool= False
         
         if len_orders !=[] and len_orders > 0:
-            max_tstamp_orders= open_orders_label_strategy['max_time_stamp']
+            max_tstamp_orders: int= open_orders_label_strategy['max_time_stamp']
             
-            minimum_waiting_time_has_passed=  self.get_basic_params().is_minimum_waiting_time_has_passed (server_time, 
+            minimum_waiting_time_has_passed: bool=  self.get_basic_params().is_minimum_waiting_time_has_passed (server_time, 
                                                                                                         max_tstamp_orders, 
                                                                                                         time_interval)
             if minimum_waiting_time_has_passed:
-                cancel_allowed= True
+                cancel_allowed: bool= True
         
         if max_tstamp_my_trades == []:
             if len_orders== [] and len_my_trades== []:
-                order_allowed= True
+                order_allowed: bool= True
                     
         else:
-            time_interval_qty= time_interval * len_my_trades
+            time_interval_qty: float= time_interval * len_my_trades
             
-            minimum_waiting_time_has_passed=  self.get_basic_params().is_minimum_waiting_time_has_passed (server_time, 
-                                                                                                        max_tstamp_my_trades, 
-                                                                                                        time_interval_qty)
+            minimum_waiting_time_has_passed: bool=  self.get_basic_params(
+                ).is_minimum_waiting_time_has_passed (server_time, 
+                                                      max_tstamp_my_trades, 
+                                                      time_interval_qty
+                                                      )
 
             if minimum_waiting_time_has_passed and len_orders== [] :
-                order_allowed= True
-                
-        if order_allowed== True:
-            
-            if strategy_config['side']=='sell':
-                params.update({"entry_price": ask_price})
-            if strategy_config['side']=='buy':
-                params.update({"entry_price": bid_price})
-
-            params.update({"side": strategy_config['side']})
-            
-            # get transaction label and update the respective parameters
-            label_open = self.get_basic_params().get_label ('open', self.strategy_label) 
-            params.update({"label": label_open})
-        
+                order_allowed: bool= True
+                        
         return dict(order_allowed= order_allowed,
                     order_parameters= [] if order_allowed== False else params,
                     cancel_allowed= cancel_allowed,
                     cancel_id= open_orders_label_strategy['order_id_max_time_stamp'])
         
     def is_send_exit_order_allowed (self,
-                                          ask_price: float,
-                                          bid_price: float,
-                                          selected_transaction: list
-                                          ) -> dict:
+                                    ask_price: float,
+                                    bid_price: float,
+                                    selected_transaction: list
+                                    ) -> dict:
         """
         """
-        return self.get_basic_params().is_send_exit_order_allowed(ask_price, bid_price, selected_transaction)
+        return self.get_basic_params(
+            ).is_send_exit_order_allowed(ask_price, 
+                                         bid_price, 
+                                         selected_transaction
+                                         )
         
