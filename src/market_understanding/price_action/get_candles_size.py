46e599e4f19b7c06264f766be0bd506ac996892d
venoajie <59521823+venoajie@users.noreply.github.com>
2023-06-21 01:42:11 +0700

    Update get_candles_size.py

--- src/market_understanding/price_action/get_candles_size.py
+++ src/market_understanding/price_action/get_candles_size.py
@@ -19,7 +19,7 @@ https://www.tradingview.com/script/uuinZwsR-Big-Bar-Strategy/
     res= await sql_executing_queries.querying_tables_item_data(tables)
     df= pd.DataFrame(res)
     df['candle_size']=df['high']-df['low']
-    df['body_size']=df['open']-df['close']
+    df['body_size']=abs(df['open']-df['close'])
     df['candle_size_avg']= df['candle_size'].rolling(period).mean()
     df['bigbar']=(df['candle_size'] >= df['candle_size_avg']*mult) & (df['body_size']>df['candle_size']*barsizeThreshold)
 
