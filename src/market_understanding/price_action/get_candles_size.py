2f81d4a6b1b909402df30525657d0234e9f01437
venoajie <59521823+venoajie@users.noreply.github.com>
2023-06-20 22:50:13 +0700

    Create get_candles_size.py

---
+++ src/market_understanding/price_action/get_candles_size.py
@@ -0,0 +1,20 @@
+from db_management import sql_executing_queries
+import asyncio
+
+async def get_dataframe_from_ohlc_tables(tables: str ='ohlc60_eth_perp_json'):
+    import pandas as pd
+    res= await sql_executing_queries.querying_tables_item_data(tables)
+    df= pd.DataFrame(res)
+    df['candle_size']=df['high']-df['low']
+    df['body_size']=df['open']-df['close']
+    df['candle_size_avg']=pd.rolling_mean(df['close'], window=10)
+    return df
+
+if __name__ == "__main__":
+
+    try:
+        asyncio.get_event_loop().run_until_complete(result())
+        
+
+    except Exception as error:
+        print(error)
