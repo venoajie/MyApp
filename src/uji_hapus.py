63e0774cbb922084f351ad206df21d685d7937c2
venoajie <59521823+venoajie@users.noreply.github.com>
2023-07-07 20:14:16 +0700

    Update uji_hapus.py

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -1,16 +1,20 @@
 from db_management import sql_executing_queries
 import asyncio
 
+
 async def result():
     from market_understanding.price_action import get_candles_size
-    candles= await get_candles_size.get_dataframe_from_ohlc_tables('ohlc1D_eth_perp_json')
+
+    candles = await get_candles_size.get_dataframe_from_ohlc_tables(
+        "ohlc1D_eth_perp_json"
+    )
     return candles
+
 
 if __name__ == "__main__":
 
     try:
         asyncio.get_event_loop().run_until_complete(result())
-        
 
     except Exception as error:
         print(error)
