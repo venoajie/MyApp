7e5ce21e4e4003ccf32023f3180ca8c9cf7bbef6
venoajie <59521823+venoajie@users.noreply.github.com>
2023-07-07 21:16:44 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -364,15 +364,15 @@ class ApplyHedgingSpot:
                         o["data"]
                         for o in detailing
                     ]
-            log.error (detailing_data)
             
-            log.info ([ast.literal_eval(str(i)) for i in (
+            cleaned_detailing_data= ([ast.literal_eval(str(i)) for i in (
                     [
                         o 
                         for o in detailing_data
                     ]
                 )]
             )
+            log.error (cleaned_detailing_data)
             result = (
                 0
                 if transactions == []
