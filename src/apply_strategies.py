12e0ce22c1a0df71cdf4dc41281b4a5bf5e95d43
venoajie <59521823+venoajie@users.noreply.github.com>
2023-07-08 12:08:49 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -331,7 +331,7 @@ class ApplyHedgingSpot:
             )
         )
         
-        log.debug([o['data']['amount_dir'] for o in detailing])
+        log.debug([o['data']for o in detailing])
         
         log.info(0 if transactions == [] else sum([(o["amount_dir"] if o['amount_dir'] != None else [
                 o['data']['amount_dir'] for o in detailing] )for o in detailing ]))
