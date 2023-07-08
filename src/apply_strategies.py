509756d101a32b9727512e423805ece8fc4dd0bc
venoajie <59521823+venoajie@users.noreply.github.com>
2023-07-08 12:02:42 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -330,9 +330,9 @@ class ApplyHedgingSpot:
                 transactions, label, detail_level
             )
         )
-        log.info(0 if transactions == [] else ([o  for o in detailing if  o['amount_dir'] != None]))
+        log.info(0 if transactions == [] else sum([(o["amount_dir"] if o['amount_dir'] != None else o['data'] ['amount_dir'] )for o in detailing ]))
 
-        return  0 if transactions == [] else sum([o["amount_dir"] for o in detailing if o['amount_dir'] != None])
+        return  0 if transactions == [] else sum([(o["amount_dir"] if o['amount_dir'] != None else o['data'] ['amount_dir'] )for o in detailing ])
 
     async def clean_up_closed_transactions(self, transactions_all) -> None:
         """ 
