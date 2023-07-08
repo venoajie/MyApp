0d42bb78c8829e2cd86a8cbf1aa94a7be8efc721
venoajie <59521823+venoajie@users.noreply.github.com>
2023-07-08 14:06:37 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -332,10 +332,10 @@ class ApplyHedgingSpot:
         )
         detailing_parsed = str_mod.parsing_sqlite_json_output([o['data']for o in detailing])
         log.debug([
-                o['data']for o in detailing_parsed])
+                o['amount_dir']for o in detailing_parsed])
         
         log.info(0 if transactions == [] else sum([(o["amount_dir"] if o['amount_dir'] != None else [
-                o['data']['amount_dir'] for o in detailing_parsed] )for o in detailing ]))
+                o['amount_dir'] for o in detailing_parsed] )for o in detailing ]))
 
         return  0 if transactions == [] else sum([(o["amount_dir"] if o['amount_dir'] != None else [
                 o['data']['amount_dir'] for o in detailing] )for o in detailing ])
