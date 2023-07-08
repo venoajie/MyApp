60c9c529321e7bb221eade9f6a64d44b1bc51330
venoajie <59521823+venoajie@users.noreply.github.com>
2023-07-08 14:32:11 +0700

    Update apply_strategies.py

--- src/apply_strategies.py
+++ src/apply_strategies.py
@@ -330,15 +330,28 @@ class ApplyHedgingSpot:
                 transactions, label, detail_level
             )
         )
-        detailing_parsed = str_mod.parsing_sqlite_json_output([o['data']for o in detailing])
-        detailing_parsed_amt_for_closed_trans= ([o["amount_dir"] for o in detailing_parsed if 'label_main' in o])
+        detailing_parsed = str_mod.parsing_sqlite_json_output(
+            [o["data"] for o in detailing]
+        )
+        detailing_parsed_amt_for_closed_trans = [
+            o["amount_dir"] for o in detailing_parsed if "label_main" in o
+        ]
         log.info(detailing_parsed_amt_for_closed_trans)
-        log.info(0 if transactions == [] else ([(o["amount_dir"]  )for o in detailing ]))
-        
-        
-        log.info(0 if transactions == [] else sum([(o["amount_dir"])for o in detailing if o['amount_dir'] != None  ]) + sum(detailing_parsed_amt_for_closed_trans))
+        log.info(0 if transactions == [] else ([(o["amount_dir"]) for o in detailing]))
 
-        return  0 if transactions == [] else sum([(o["amount_dir"] if o['amount_dir'] != None else detailing_parsed_amt_for_closed_trans )for o in detailing ])
+        log.info(
+            0
+            if transactions == []
+            else sum([(o["amount_dir"]) for o in detailing if o["amount_dir"] != None])
+            + sum(detailing_parsed_amt_for_closed_trans)
+        )
+
+        return (
+            0
+            if transactions == []
+            else sum([(o["amount_dir"]) for o in detailing if o["amount_dir"] != None])
+            + sum(detailing_parsed_amt_for_closed_trans)
+        )
 
     async def clean_up_closed_transactions(self, transactions_all) -> None:
         """ 
