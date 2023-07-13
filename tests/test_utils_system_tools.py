8b4edc8aca8f7ece081b11fe4c139a7e14d1ee55
venoajie <59521823+venoajie@users.noreply.github.com>
2023-07-13 18:38:06 +0700

    Update test_utils_system_tools.py

--- tests/test_utils_system_tools.py
+++ tests/test_utils_system_tools.py
@@ -1,12 +1,10 @@
 from src.utilities import system_tools
 
+
 @system_tools.exception_handler
 def test_divide(x, y):
     result = x / y
     return result
-test_divide(10, 0)  
 
 
-
-
-
+test_divide(10, 0)
