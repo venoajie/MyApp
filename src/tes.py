c3c086487334186fb5961fd77629507531e8f72b
venoajie <59521823+venoajie@users.noreply.github.com>
2023-08-24 10:21:59 +0700

    Update tes.py

--- src/tes.py
+++ src/tes.py
@@ -10,6 +10,19 @@ def adder(n):
 add_five = adder(5)
 add_ten = adder(10)
 
-print(add_five())  # Output: 8
+print(add_five(5))  # Output: 8
 print(add_five(3))  # Output: 8
 print(add_ten(3))  # Output: 13
+
+f=['a','b','c','d']
+syarat=['a','b','d']
+res= [o for o in f if o in syarat]
+print(res)
+
+from strategies import entries_exits
+
+strategies = entries_exits.strategies
+
+strategy_attr = [o['strategy'] for o in strategies if o["contribute_to_hedging"] == True]
+
+print(strategy_attr)
