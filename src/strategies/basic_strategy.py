8c170d591a8c09ac55a1209771322c1b70ff3214
venoajie <59521823+venoajie@users.noreply.github.com>
2023-08-25 08:32:08 +0700

    Update basic_strategy.py

--- src/strategies/basic_strategy.py
+++ src/strategies/basic_strategy.py
@@ -33,11 +33,9 @@ async def cleaned_up_ohlc(limit: int= 100, table: str = "ohlc1_eth_perp_json") -
     
     # reversing result as price will be processed from the latest to current one
     ohlc.reverse()
-    print (f'ohlc {ohlc}')
 
     # exclude last price to minimize its impact to TA calc
     ohlc_reversed= ohlc[:limit-1]
-    print (f'ohlc-1 {ohlc_reversed}')
     
     return dict(
             ohlc_reversed = ohlc_reversed,
@@ -54,7 +52,7 @@ async def get_ema (ohlc, ratio: float = 0.9) -> dict:
     return  round(sum([ratio * ohlc[-x - 1] * ((1 - ratio) ** x) for x in range(len(ohlc))]), 2)
             
 
-async def market_condition(threshold, limit: int = 100, ratio: float = 0.9, table: str = "ohlc1_eth_perp_json") -> dict:
+async def get_market_condition(threshold, limit: int = 100, ratio: float = 0.9, table: str = "ohlc1_eth_perp_json") -> dict:
     """
     """
     ohlc= await cleaned_up_ohlc (limit, table)
