2a2050d36246730faf95cb749284dffcb8695a94
venoajie <59521823+venoajie@users.noreply.github.com>
2023-08-27 16:17:59 +0700

    Update string_modification.py

--- src/utilities/string_modification.py
+++ src/utilities/string_modification.py
@@ -234,14 +234,14 @@ def parsing_label(label: str, integer: int = None) -> dict:
 def get_net_sum_strategy_super_main(my_trades_open_sqlite: list, label: str) -> float:
     """ """
     print (f' get_net_sum_strategy_super_main my_trades_open_sqlite {my_trades_open_sqlite}')
-    print (f' sum {sum(
+    print ({sum(
             [
                 o["amount_dir"]
                 for o in my_trades_open_sqlite
                 if parsing_label(o["label_main"])["super_main"]
                 == parsing_label(label)["super_main"]
             ]
-        )}')
+        )})
     return (
         0
         if my_trades_open_sqlite == []
