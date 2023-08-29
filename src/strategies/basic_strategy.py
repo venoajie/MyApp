b57116da48d8b789a45db0b1b7a729c5c2d8e596
venoajie <59521823+venoajie@users.noreply.github.com>
2023-08-29 23:01:10 +0700

    Update basic_strategy.py

--- src/strategies/basic_strategy.py
+++ src/strategies/basic_strategy.py
@@ -476,14 +476,7 @@ class BasicStrategy:
         no_outstanding_order: bool = len_orders == 0
 
         order_allowed: bool = tp_price_reached and no_outstanding_order
-    #!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC    
-    
-        trade_seq = params["label"]
-        print(f"trade_seq {trade_seq}")
-
-        order_has_sent_before = await self.is_order_has_sent_before(trade_seq)
-    #!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC    
-
+        
         if order_allowed:
 
             params.update({"instrument": transaction["instrument_name"]})
