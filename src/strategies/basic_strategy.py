1a06fa63474440a2c831d642ec1eb37731e2161d
venoajie <59521823+venoajie@users.noreply.github.com>
2023-08-29 13:10:03 +0700

    Update basic_strategy.py

--- src/strategies/basic_strategy.py
+++ src/strategies/basic_strategy.py
@@ -429,7 +429,8 @@ class BasicStrategy:
         my_trades_attributes_closed = get_my_trades_attributes_closed[
             "transactions_strategy_label"
         ]
-        print(f"my_trades_attributes_closed {my_trades_attributes_closed} trade_seq {trade_seq}")
+        trade_seqs= [o["trade_seq"] for o in my_trades_attributes_closed]
+        print(f"my_trades_attributes_closed {my_trades_attributes_closed} trade_seq {trade_seq} trade_seqs {trade_seqs}")
         print([o for o in my_trades_attributes_closed if trade_seq in o["trade_seq"]])
         trade_seq_is_exist: list = False if my_trades_attributes_closed == [] else [
             o for o in my_trades_attributes_closed if trade_seq in o["trade_seq"]
