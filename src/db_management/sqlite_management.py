f1a13d69b8b0245463a0e5a1b0630d78ccb5c2fb
venoajie <59521823+venoajie@users.noreply.github.com>
2024-06-03 00:27:18 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -581,8 +581,8 @@ def querying_open_interest(
     price: float = "close", table: str = "ohlc1_eth_perp_json", limit: int = None
 ) -> str:
 
-    all_data = f"""SELECT tick, JSON_EXTRACT (data, '$.volume') AS volume, JSON_EXTRACT (data, '$.{price}')  AS close, open_interest, \
-        (open_interest - LAG (open_interest, 1, 0) OVER (ORDER BY tick)) as delta_oi FROM {table}"""
+    all_data = f"""SELECT tick, JSON_EXTRACT (data, '$.volume') AS volume, open_interest, \
+        (open_interest - LAG (open_interest, 1, 0) OVER (ORDER BY tick)) as delta_oi FROM {table}, JSON_EXTRACT (data, '$.{price}')  AS close ORDER BY tick DESC"""
 
     return all_data if limit == None else f"""{all_data} limit {limit}"""
 
