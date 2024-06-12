42f8425cbb2a69410eb19cfdc4db5a6c39f2764d
venoajie <venoajie@googlemail.com>
2024-06-12 21:27:50 +0700

    Update uji_hapus copy.py

--- src/uji_hapus copy.py
+++ src/uji_hapus copy.py
@@ -3014,30 +3014,9 @@ my_trades_open = [
         "amount": 1.0,
     },
 ]
-
-my_trades_path = system_tools.provide_path_for_file("myTrades", "eth", "open")
-
-my_trades_path_open_recovery = system_tools.provide_path_for_file(
-    "myTrades", "eth", "open-recovery-point"
-)
-my_trades = pickling.read_data(my_trades_path_open_recovery)
-
-net = number_modification.net_position(my_trades_open)
-print(net)
-
-
-async def main() -> list:
-    """ """
-    await sqlite_management.insert_tables("my_trades_all_json", my_trades_open)
-    await sqlite_management.insert_tables("my_trades_all", my_trades_open)
-
-
-if __name__ == "__main__":
-
-    try:
-        pickling.replace_data(my_trades_path, my_trades_open, True)
-        pickling.replace_data(my_trades_path_open_recovery, my_trades_open, True)
-        asyncio.get_event_loop().run_until_complete(main())
-
-    except Exception as error:
-        print(error)
+label='marketMakerShort-open-1718128706842'
+params= [{'label': 'marketMakerShort-open-1718128706842', 'take_profit': None}, {'label': 'marketMakerLong-open-1718128716991', 'take_profit': None}]
+additional_params_label = [
+            o for o in params if label in o["label"]
+        ]
+print(additional_params_label)
