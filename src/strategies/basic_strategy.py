c1ca0eb2b99d8092e54e57b7ee90ac121e2f345a
venoajie <59521823+venoajie@users.noreply.github.com>
2024-06-21 20:43:43 +0700

    Update basic_strategy.py

--- src/strategies/basic_strategy.py
+++ src/strategies/basic_strategy.py
@@ -525,7 +525,7 @@ class BasicStrategy:
         """ """
 
         result: list = await querying_label_and_size(table)
-        log.error(f"result {result}")
+        #log.error(f"result {result}")
 
         # clean up result with no label main
         result_cleaned = [o for o in result if o["label"] != None]
