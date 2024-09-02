4e7b1e52ca7aa69340930506aeada5576c2f3ca9
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-03 05:24:01 +0700

    Update technical_analysis.py

--- src/market_understanding/technical_analysis.py
+++ src/market_understanding/technical_analysis.py
@@ -194,6 +194,7 @@ async def get_market_condition(instrument,
     #log.info (f"currency.lower() {currency_lower}")
     
     last_tick_from_prev_TA = get_last_tick_from_prev_TA(TA_result_data)
+    log.info (f"instrument {instrument} last_tick_from_prev_TA {last_tick_from_prev_TA} current_tick {current_tick}")
     log.info (f"last_tick_from_prev_TA == 0 {last_tick_from_prev_TA == 0}")
     log.error (f"current_tick > last_tick_from_prev_TA {current_tick > last_tick_from_prev_TA} current_tick {current_tick} ")
 
