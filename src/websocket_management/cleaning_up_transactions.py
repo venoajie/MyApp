7fd80edc45a52db647e1218a6bccd4933fff7955
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-04 20:32:11 +0700

    Update cleaning_up_transactions.py

--- src/websocket_management/cleaning_up_transactions.py
+++ src/websocket_management/cleaning_up_transactions.py
@@ -100,6 +100,7 @@ async def reconciling_between_db_and_exchg_data(
         for order_id in unrecorded_order_id:
 
             transaction = [o for o in trades_from_exchange if o["order_id"] == order_id]
+            print(f"transaction {transaction}")
 
             label = [
                 o["label"] for o in trades_from_exchange if o["order_id"] == order_id
