70bc390bf2b7317576ae1e73cbef44dfcddc4e3f
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-05 12:05:53 +0700

    Update cleaning_up_transactions.py

--- src/websocket_management/cleaning_up_transactions.py
+++ src/websocket_management/cleaning_up_transactions.py
@@ -50,7 +50,7 @@ async def get_unrecorded_order_id(
 
     unrecorded_order_id = find_unique_elements(
         combined_closed_open, from_exchange_order_id
-    )q
+    )
 
     unrecorded_order_id = list(
         set(from_exchange_order_id).difference(combined_closed_open)
