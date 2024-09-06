5a5f6f3fde5a7ee99d74e0d35a11908993c81d8e
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-06 09:11:19 +0700

    Update cleaning_up_transactions.py

--- src/websocket_management/cleaning_up_transactions.py
+++ src/websocket_management/cleaning_up_transactions.py
@@ -36,6 +36,7 @@ async def get_unrecorded_order_id(instrument,
     """ """
 
     from_sqlite_closed_order_id = [o["order_id"] for o in from_sqlite_closed]
+    log.critical (f"{instrument}")
     log.info (f"from_sqlite_closed_order_id {from_sqlite_closed_order_id}")
 
     from_sqlite_open_order_id = [o["order_id"] for o in from_sqlite_open]  
@@ -52,6 +53,8 @@ async def get_unrecorded_order_id(instrument,
     
     combined_closed_open = from_sqlite_open_order_id + from_sqlite_closed_order_id
 
+    log.warning (f"combined_closed_open {combined_closed_open}")
+
     unrecorded_order_id = find_unique_elements(
         combined_closed_open, from_exchange_order_id
     )
@@ -60,7 +63,7 @@ async def get_unrecorded_order_id(instrument,
     unrecorded_order_id = list(
         set(from_exchange_order_id).difference(combined_closed_open)
     )
-    log.debug (f"unrecorded_order_id {unrecorded_order_id}")
+    log.debug (f"unrecorded_order_id final {unrecorded_order_id}")
 
     return unrecorded_order_id
 
