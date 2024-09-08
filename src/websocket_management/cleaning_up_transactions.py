d87c12e1ba516c282e4b873349855efd78ccf888
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-08 16:18:47 +0700

    Update cleaning_up_transactions.py

--- src/websocket_management/cleaning_up_transactions.py
+++ src/websocket_management/cleaning_up_transactions.py
@@ -140,6 +140,12 @@ async def remove_duplicated_elements (instrument_name) -> None:
         my_trades_open_all: list = await executing_general_query_with_single_filter("my_trades_all_json", instrument_name)     
 
         for trade_id in duplicated_trade_id:
+            
+            log.info ([
+                    o["id"]
+                    for o in my_trades_open_all
+                    if o["trade_id"] == trade_id
+                ])
             id = max(
                 [
                     o["id"]
