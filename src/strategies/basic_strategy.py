8652337a0cef42941d935fdc350a00bbe61190d5
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-09 20:29:46 +0700

    update

--- src/strategies/basic_strategy.py
+++ src/strategies/basic_strategy.py
@@ -502,9 +502,9 @@ async def get_additional_params_for_open_label(trade: list, label: str) -> None:
 
     params = await executing_query_with_return(additional_params)
 
-    additional_params_label = [o for o in params if label in o["label"]][0]
-    
-    log.error (f"additional_params_label {additional_params_label}")
+    log.error (f"params {params}")
+
+    additional_params_label = [o for o in params if label in o["label"]][0]    
     
     if "take_profit" not in trade:
         trade.update({"take_profit": additional_params_label["take_profit"]})
