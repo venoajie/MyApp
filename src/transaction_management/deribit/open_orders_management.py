0db20a6de398576a34225b23ac35beec9c3dad95
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-10 12:42:59 +0700

    update

--- src/transaction_management/deribit/open_orders_management.py
+++ src/transaction_management/deribit/open_orders_management.py
@@ -13,93 +13,90 @@ def telegram_bot_sendtext(bot_message, purpose: str = "general_error") -> None:
     return telegram_app.telegram_bot_sendtext(bot_message, purpose)
 
 
-async def manage_orders(orders: dict) -> None:
-    log.debug(f"{orders}")
+async def manage_orders(order: dict) -> None:
 
-    for order in orders:
+    log.error(f"{order}")
 
-        log.error(f"{order}")
+    #! ##############################################################################
 
-        #! ##############################################################################
+    open_orders_sqlite = await sqlite_management.executing_label_and_size_query(
+        "orders_all_json"
+    )
 
+    if "oto_order_ids" in order:
+        # get the order state
+        oto_order_ids = order["oto_order_ids"]
+        log.error(f"oto_order_ids {oto_order_ids}")
+
+    if "is_primary_otoco" in order:
+        # get the order state
+        is_primary_otoco = order["is_primary_otoco'"]
+        log.error(f"is_primary_otoco {is_primary_otoco}")
+
+    if "is_secondary_oto" in order:
+        # get the order state
+        is_secondary_oto = order["is_secondary_oto'"]
+        log.error(f"is_secondary_oto {is_secondary_oto}")
+
+    if "oco_ref" in order:
+        # get the order state
+        oco_ref = order["oco_ref'"]
+        log.error(f"oco_ref {oco_ref}")
+
+    if "oto_order_ids" in order:
+        # get the order state
+        oto_order_ids = order["oto_order_ids"]
+        log.error(f"oto_order_ids {oto_order_ids}")
+
+    if "trade_seq" not in order:
+        # get the order state
+        order_state = order["order_state"]
+
+    if "trade_seq" in order:
+
+        # get the order state
+        order_state = order["state"]
+
+    log.error(f"ORDER STATE {order_state}")
+
+    if (
+        order_state == "cancelled"
+        or order_state == "filled"
+        or order_state == "triggered"
+    ):
+
+        order_id = (
+            order["order_id"] if order_state != "triggered" else ["stop_order_id'"]
+        )
+
+        # open_orders_sqlite =  await syn.querying_all('orders_all_json')
         open_orders_sqlite = await sqlite_management.executing_label_and_size_query(
             "orders_all_json"
         )
+        # open_orders_sqlite_list_data =  open_orders_sqlite['list_data_only']
 
-        if "oto_order_ids" in order:
-            # get the order state
-            oto_order_ids = order["oto_order_ids"]
-            log.error(f"oto_order_ids {oto_order_ids}")
+        is_order_id_in_active_orders = [
+            o for o in open_orders_sqlite if o["order_id"] == order_id
+        ]
 
-        if "is_primary_otoco" in order:
-            # get the order state
-            is_primary_otoco = order["is_primary_otoco'"]
-            log.error(f"is_primary_otoco {is_primary_otoco}")
+        where_filter = f"order_id"
+        if is_order_id_in_active_orders == []:
+            order_id = order["label"]
+            where_filter = f"label_main"
 
-        if "is_secondary_oto" in order:
-            # get the order state
-            is_secondary_oto = order["is_secondary_oto'"]
-            log.error(f"is_secondary_oto {is_secondary_oto}")
+        log.critical(f" deleting {order_id}")
+        await sqlite_management.deleting_row(
+            "orders_all_json",
+            "databases/trading.sqlite3",
+            where_filter,
+            "=",
+            order_id,
+        )
 
-        if "oco_ref" in order:
-            # get the order state
-            oco_ref = order["oco_ref'"]
-            log.error(f"oco_ref {oco_ref}")
+    if (
+        order_state == "open"
+        or order_state == "untriggered"
+        or order_state == "triggered"
+    ):
 
-        if "oto_order_ids" in order:
-            # get the order state
-            oto_order_ids = order["oto_order_ids"]
-            log.error(f"oto_order_ids {oto_order_ids}")
-
-        if "trade_seq" not in order:
-            # get the order state
-            order_state = order["order_state"]
-
-        if "trade_seq" in order:
-
-            # get the order state
-            order_state = order["state"]
-
-        log.error(f"ORDER STATE {order_state}")
-
-        if (
-            order_state == "cancelled"
-            or order_state == "filled"
-            or order_state == "triggered"
-        ):
-
-            order_id = (
-                order["order_id"] if order_state != "triggered" else ["stop_order_id'"]
-            )
-
-            # open_orders_sqlite =  await syn.querying_all('orders_all_json')
-            open_orders_sqlite = await sqlite_management.executing_label_and_size_query(
-                "orders_all_json"
-            )
-            # open_orders_sqlite_list_data =  open_orders_sqlite['list_data_only']
-
-            is_order_id_in_active_orders = [
-                o for o in open_orders_sqlite if o["order_id"] == order_id
-            ]
-
-            where_filter = f"order_id"
-            if is_order_id_in_active_orders == []:
-                order_id = order["label"]
-                where_filter = f"label_main"
-
-            log.critical(f" deleting {order_id}")
-            await sqlite_management.deleting_row(
-                "orders_all_json",
-                "databases/trading.sqlite3",
-                where_filter,
-                "=",
-                order_id,
-            )
-
-        if (
-            order_state == "open"
-            or order_state == "untriggered"
-            or order_state == "triggered"
-        ):
-
-            await sqlite_management.insert_tables("orders_all_json", order)
+        await sqlite_management.insert_tables("orders_all_json", order)
