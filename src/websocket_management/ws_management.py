3607a41dd1f7c04f9c948c62a2e581f4056c4053
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-13 09:29:18 +0700

    Update ws_management.py

--- src/websocket_management/ws_management.py
+++ src/websocket_management/ws_management.py
@@ -291,8 +291,7 @@ async def updated_open_orders_database(currency) -> None:
     # resupply sub account db
     sub_accounts = await get_sub_account(currency)
     
-    log.error(f"sub_accounts{sub_accounts}")
-    open_orders_from_sub_accounts= sub_accounts["open_orders"]
+    open_orders_from_sub_accounts= sub_accounts[0]["open_orders"]
     open_orders_from_sub_accounts_order_id= [o["order_id"] for o in open_orders_from_sub_accounts]
     
     
