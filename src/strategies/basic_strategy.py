3aa80afed98d672e978e99b80b4c6866e58600fd
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-14 13:19:39 +0700

    Update basic_strategy.py

--- src/strategies/basic_strategy.py
+++ src/strategies/basic_strategy.py
@@ -657,6 +657,8 @@ def check_db_consistencies (instrument_name: str,
     len_from_sqlite_open= len(order_from_sqlite_open)
     
     len_open_orders_from_sub_accounts=len(open_orders_from_sub_accounts)
+    
+    log.error (f"positions_from_sub_account {positions_from_sub_account}")
         
     sum_my_trades_sqlite = 0 if  trades_from_sqlite == [] else sum([o["amount"] for o in trades_from_sqlite])
 
