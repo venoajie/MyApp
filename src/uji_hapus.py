f772a7cea03e217f39b9efb1e40db2225e6aa380
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-15 15:22:44 +0700

    Update uji_hapus.py

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -4,21 +4,19 @@ from utilities.system_tools import (
     catch_error_message)
 import sqlite3
 
-from configuration.config import main_dotenv
+def progress(status, remaining, total):
+    log.warning(f'Copied {total-remaining} of {total} pages...')
+    
+def back_up_db():
+    src = sqlite3.connect("databases/trading.sqlite3")
+    dst = sqlite3.connect("databases/trading.bak")
+    with dst:
+        src.backup(dst)
+    dst.close()
+    src.close()
 
-def parse_dotenv(sub_account) -> dict:
-    return main_dotenv(sub_account)
-
-        
-async def get_private_data() -> list:
-    """
-    Provide class object to access private get API
-    """
-
-    sub_account = "backblaze-backup-trading-sqlite"
-    client_id: str = parse_dotenv(sub_account)["client_id"]
-    client_secret: str = parse_dotenv(sub_account)["client_secret"]
-    return 
+    catch_error_message(
+    "back up done")
 
         
 if __name__ == "__main__":
