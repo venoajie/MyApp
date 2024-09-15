9f041b325632f7dd86a23d69888e65aa2e2173fc
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-15 15:29:28 +0700

    Update sync_with_remote.sh

--- src/sync_with_remote.sh
+++ src/sync_with_remote.sh
@@ -5,7 +5,7 @@ while true; do
 
     if test -f databases/exchanges/deribit/transactions/eth-myTrades-open-recovery-point.pkl;
         then
-                echo "file exists"
+                 echo "file exists"
                 echo "Moving local  files to remote..."
                 rclone sync  databases/exchanges/deribit/transactions/eth-myTrades-open-recovery-point.pkl oci:bucket-20230107-0704
                 rclone sync  databases/exchanges/deribit/transactions/eth-myTrades-open-recovery-point.pkl b2:MyAppTrading
