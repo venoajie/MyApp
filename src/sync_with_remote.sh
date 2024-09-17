3633dd0489b125bbbfef604d7adddde8ecaa1aa3
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-17 14:47:58 +0700

    Update sync_with_remote.sh

--- src/sync_with_remote.sh
+++ src/sync_with_remote.sh
@@ -5,13 +5,13 @@ while true; do
 
     if test -f databases/trdg.bak;
         then
-                 echo "file exists"
+                echo "file exists"
                 echo "Moving local  files to remote..."
                 #rclone sync  databases/exchanges/deribit/transactions/eth-myTrades-open-recovery-point.pkl oci:bucket-20230107-0704
                 rclone sync databases/trdg.bak remote:/remote-sqlite
                 
-                echo "sync_with_remote: sleep 30 minutes"
-                sleep 60m
+                echo "sync_with_remote: sleep 15 minutes"
+                sleep 15m
 
         else
                 echo "sync_with_remote: file does not exist"
