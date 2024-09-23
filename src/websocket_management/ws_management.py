faecd3fe6bbf19dc693892efc36eb7f124fae2ae
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-23 22:51:10 +0700

    Update ws_management.py

--- src/websocket_management/ws_management.py
+++ src/websocket_management/ws_management.py
@@ -446,11 +446,11 @@ async def check_db_consistencies_and_clean_up_imbalances(currency: str, sub_acco
                                     entry_price=tickers["best_bid_price"]
                                     
                                 basic_closing_paramaters.update({"entry_price":entry_price})
-                                basic_closing_paramaters.update({"amount":abs(basic_closing_paramaters["amount"])})
-                                
-                                await send_limit_order(basic_closing_paramaters)  
+                                basic_closing_paramaters.update({"amount":abs(basic_closing_paramaters["size"])})
                                 
                                 log.error (f"basic_closing_paramaters {basic_closing_paramaters}")
+                                await send_limit_order(basic_closing_paramaters)  
+                                
                             #log.error (f"my_trades_instrument_data {transaction}")
                         
                             
