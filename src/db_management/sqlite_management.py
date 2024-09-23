d9527e527e6eacacb602cac42cd9ffbf74da17c8
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-23 10:35:31 +0700

    Update sqlite_management.py

--- src/db_management/sqlite_management.py
+++ src/db_management/sqlite_management.py
@@ -475,7 +475,12 @@ def querying_based_on_currency_or_instrument_and_strategy (table: str,
         standard_columns= F"{standard_columns}, trade_id, type"
         
     if columns != "standard":
-        standard_columns= (','.join(str(f"""{i}{("_dir as amount") if i=="amount" else ""}""") for i in columns))
+        
+        if "data" in columns:
+            standard_columns= f"data"
+            
+        else:
+            standard_columns= (','.join(str(f"""{i}{("_dir as amount") if i=="amount" else ""}""") for i in columns))
         
     where_clause= f"WHERE (instrument_name LIKE '%{currency_or_instrument}%')"
 
