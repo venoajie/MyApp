906d8d89fa6f22e55140a0f7d52cac3c1a6884e8
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-25 15:39:12 +0700

    .

--- src/uji_hapus copy.py
+++ src/uji_hapus copy.py
@@ -2,14 +2,23 @@
 def extract_currency_from_text(words: str) -> str:
     """
     Extracting currency from channel message
+    
+     some variables:
+     chart.trades.BTC-PERPETUAL.1
+     incremental_ticker.BTC-4OCT24
     """
     if "."in words:
           filter1= (words.partition('.')[2]).lower()
-    
+          print(filter1)
+          
+          if "."in filter1:
+            filter1= (filter1.partition('.')[2]).lower()
+            print(filter1)
+      
     else:
           filter1= (words.partition('.')[0]).lower()
         
     return (filter1.partition('-')[0]).lower()
 
-print(extract_currency_from_text("BTC-PERPETUAL"))
+print(extract_currency_from_text("chart.trades.BTC-PERPETUAL.1"))
         
--- src/utilities/string_modification.py
+++ src/utilities/string_modification.py
@@ -127,12 +127,20 @@ def get_unique_elements(data1: list, data2: list) -> list:
 
 def extract_currency_from_text(words: str) -> str:
     """
-    Extracting currency from channel message
+
+     some variables:
+     chart.trades.BTC-PERPETUAL.1
+     incremental_ticker.BTC-4OCT24
     """
-    
+
     if "."in words:
           filter1= (words.partition('.')[2]).lower()
-    
+          print(filter1)
+          
+          if "."in filter1:
+            filter1= (filter1.partition('.')[2]).lower()
+            print(filter1)
+      
     else:
           filter1= (words.partition('.')[0]).lower()
         
