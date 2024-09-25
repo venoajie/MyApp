689112017374899fbd1c0c365239224e0b9ca833
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-25 10:14:29 +0700

    Update uji_hapus.py

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -3,7 +3,7 @@ import tomli
   
   
 # Opening a Toml file using tomlib 
-with open("MyApp/src/strategies/config_strategies.toml","rb") as toml: 
+with open("home/live/MyApp/src/strategies/config_strategies.toml","rb") as toml: 
     toml_dict = tomli.load(toml) 
   
 # Printing the entire fetched toml file 
