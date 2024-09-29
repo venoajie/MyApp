cf58a19cb243351b5cfe8424c1217339c6b0c30b
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-29 11:23:17 +0700

    .

--- src/websocket_management/allocating_ohlc.py
+++ src/websocket_management/allocating_ohlc.py
@@ -76,7 +76,6 @@ async def ohlc_result_per_time_frame(
     data_orders,
     TABLE_OHLC1: str,
     WHERE_FILTER_TICK: str = "tick",
-    DATABASE: str = "databases/trading.sqlite3",
 ) -> None:
 
     last_tick_query_ohlc1: str = querying_arithmetic_operator (WHERE_FILTER_TICK, "MAX", TABLE_OHLC1)
@@ -84,15 +83,15 @@ async def ohlc_result_per_time_frame(
     last_tick1_fr_sqlite: int = await last_tick_fr_sqlite (last_tick_query_ohlc1)
 
     last_tick_fr_data_orders: int = data_orders ["tick"]
-    log.debug (f"data_orders {instrument_ticker} {data_orders}")
+    #log.debug (f"data_orders {instrument_ticker} {data_orders}")
 
     # refilling current ohlc table with updated data
 
     refilling_current_ohlc_table_with_updated_streaming_data = last_tick1_fr_sqlite == last_tick_fr_data_orders
     insert_new_ohlc_and_replace_previous_ohlc_using_fix_data = last_tick_fr_data_orders > last_tick1_fr_sqlite
-    log.warning (f"last_tick1_fr_sqlite {last_tick1_fr_sqlite} last_tick_fr_data_orders {last_tick_fr_data_orders}")
-    log.debug (f"refilling_current_ohlc_table_with_updated_streaming_data {refilling_current_ohlc_table_with_updated_streaming_data} ")
-    log.error (f"insert_new_ohlc_and_replace_previous_ohlc_using_fix_data {insert_new_ohlc_and_replace_previous_ohlc_using_fix_data}")
+    #ning (f"last_tick1_fr_sqlite {last_tick1_fr_sqlite} last_tick_fr_data_orders {last_tick_fr_data_orders}")
+    #log.debug (f"refilling_current_ohlc_table_with_updated_streaming_data {refilling_current_ohlc_table_with_updated_streaming_data} ")
+    #log.error (f"insert_new_ohlc_and_replace_previous_ohlc_using_fix_data {insert_new_ohlc_and_replace_previous_ohlc_using_fix_data}")
     
     if refilling_current_ohlc_table_with_updated_streaming_data:
     
@@ -112,7 +111,10 @@ def currency_inline_with_database_address (currency: str, database_address: str)
     return currency.lower()  in str(database_address)
 
 
-async def inserting_open_interest(currency, DATABASE, WHERE_FILTER_TICK, TABLE_OHLC1, data_orders) -> None:
+async def inserting_open_interest(currency, 
+                                  WHERE_FILTER_TICK,
+                                  TABLE_OHLC1, 
+                                  data_orders) -> None:
     """ """
     try:
 
