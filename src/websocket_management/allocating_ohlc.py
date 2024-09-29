071e9b0fc44f4f66e81866e115c69f31f09e9e09
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-29 10:13:52 +0700

    Update allocating_ohlc.py

--- src/websocket_management/allocating_ohlc.py
+++ src/websocket_management/allocating_ohlc.py
@@ -46,6 +46,27 @@ async def last_tick_fr_sqlite(last_tick_query_ohlc1) -> int:
         )
     return last_tick1_fr_sqlite[0]["MAX (tick)"]
 
+async def replace_previous_ohlc_using_fix_data(TABLE_OHLC1, last_tick_query_ohlc1, WHERE_FILTER_TICK) -> int:
+    """ """
+    try:
+
+        ohlc_endPoint = f" https://deribit.com/api/v2/public/get_tradingview_chart_data?end_timestamp={last_tick_fr_data_orders}&instrument_name={instrument_ticker}&resolution=1&start_timestamp={last_tick1_fr_sqlite}"
+
+        ohlc_request = requests.get(ohlc_endPoint).json()["result"]
+        result = transform_nested_dict_to_list(ohlc_request)
+
+        # update last tick
+        last_tick1_fr_sqlite = await last_tick_fr_sqlite(last_tick_query_ohlc1)
+
+        await update_status_data(TABLE_OHLC1, "data", last_tick1_fr_sqlite, WHERE_FILTER_TICK, result, "is")
+        
+    except Exception as error:
+        await raise_error_message(
+            error,
+            "Capture market data - failed to fetch last_tick_fr_sqlite",
+        )
+    return last_tick1_fr_sqlite[0]["MAX (tick)"]
+
 
 async def ohlc_result_per_time_frame(
     message_channel,
@@ -67,40 +88,21 @@ async def ohlc_result_per_time_frame(
 
     if (TABLE_OHLC1 != None
     ):
-        #print(f"allocating ohlc message_channel {message_channel} instrument_ticker {instrument_ticker}")
-        #print(f"last_tick_fr_data_orders {last_tick_fr_data_orders} last_tick1_fr_sqlite {last_tick1_fr_sqlite}")
-        #print(message_channel == f"chart.trades.{instrument_ticker}.1")
 
         if message_channel == f"chart.trades.{instrument_ticker}.1":
+            
+            log.error (f"{last_tick1_fr_sqlite == last_tick_fr_data_orders} last_tick1_fr_sqlite {last_tick1_fr_sqlite} last_tick_fr_data_orders {last_tick_fr_data_orders}")
 
             # refilling current ohlc table with updated data
             if last_tick1_fr_sqlite == last_tick_fr_data_orders:
                 
-                #log.debug (f"data_orders {data_orders}")
+                log.debug (f"data_orders {data_orders}")
                 await update_status_data(TABLE_OHLC1, "data", last_tick1_fr_sqlite, WHERE_FILTER_TICK, data_orders, "is")
                 
             # new tick ohlc
             else:
-                
-                ohlc_endPoint = f" https://deribit.com/api/v2/public/get_tradingview_chart_data?end_timestamp={last_tick_fr_data_orders}&instrument_name={instrument_ticker}&resolution=1&start_timestamp={last_tick1_fr_sqlite}"
-
-                ohlc_request = requests.get(ohlc_endPoint).json()["result"]
-                result = transform_nested_dict_to_list(ohlc_request)
-                
-                
-                # prepare query
-                open_interest_last_value_query = querying_last_open_interest_tick (last_tick1_fr_sqlite, TABLE_OHLC1)
-
-                # get current oi
-                open_interest_last_value = await last_open_interest_tick_fr_sqlite (open_interest_last_value_query)
-
-                # insert new ohlc data
-                await insert_tables(TABLE_OHLC1, data_orders)
-
-                # update last tick
-                last_tick1_fr_sqlite = await last_tick_fr_sqlite(last_tick_query_ohlc1)
-
-                await update_status_data(TABLE_OHLC1, "open_interest", last_tick1_fr_sqlite, WHERE_FILTER_TICK, result, "is")
+                log.warning (f"data_orders {data_orders}")
+                await replace_previous_ohlc_using_fix_data (TABLE_OHLC1, last_tick_query_ohlc1, WHERE_FILTER_TICK)
                 
 def currency_inline_with_database_address (currency: str, database_address: str) -> bool:
     return currency.lower()  in str(database_address)
