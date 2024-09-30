36d8c43a4297a2f4da68981fddfbf00e00783693
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-30 10:56:08 +0700

    .

--- src/uji_hapus copy.py
+++ src/uji_hapus copy.py
@@ -1,24 +1,9 @@
+data=  [{'tick_size_steps': [], 
+         'quote_currency': 'USD', 'min_trade_amount': 10.0, 'counter_currency': 'USD', 'settlement_currency': 'BTC', 
+         'block_trade_min_trade_amount': 200000, 'block_trade_commission': 0.00025, 'max_liquidation_commission': 0.0075, 
+         'max_leverage': 25, 'future_type': 'reversed', 'creation_timestamp': 1726819213000, 'settlement_period': 'week',
+         'instrument_id': 370613, 'base_currency': 'BTC', 'block_trade_tick_size': 0.01, 'tick_size': 2.5, 'contract_size': 10.0, 'is_active': True, 
+         'expiration_timestamp': 1728028800000, 'instrument_type': 'reversed', 'taker_commission': 0.0005, 'maker_commission': -0.0001, 
+         'instrument_name': 'BTC-4OCT24', 'kind': 'future', 'rfq': False, 'price_index': 'btc_usd'}, {'tick_size_steps': [], 'quote_currency': 'USD', 'min_trade_amount': 10.0, 'counter_currency': 'USD', 'settlement_currency': 'BTC', 'block_trade_min_trade_amount': 200000, 'block_trade_commission': 0.00025, 'max_liquidation_commission': 0.0075, 'max_leverage': 25, 'future_type': 'reversed', 'creation_timestamp': 1727424018000, 'settlement_period': 'week', 'instrument_id': 373193, 'base_currency': 'BTC', 'block_trade_tick_size': 0.01, 'tick_size': 2.5, 'contract_size': 10.0, 'is_active': True, 'expiration_timestamp': 1728633600000, 'instrument_type': 'reversed', 'taker_commission': 0.0005, 'maker_commission': -0.0001, 'instrument_name': 'BTC-11OCT24', 'kind': 'future', 'rfq': False, 'price_index': 'btc_usd'}, {'tick_size_steps': [], 'quote_currency': 'USD', 'min_trade_amount': 10.0, 'counter_currency': 'USD', 'settlement_currency': 'BTC', 'block_trade_min_trade_amount': 200000, 'block_trade_commission': 0.00025, 'max_liquidation_commission': 0.0075, 'max_leverage': 50, 'future_type': 'reversed', 'creation_timestamp': 1534242287000, 'settlement_period': 'perpetual', 'instrument_id': 210838, 'base_currency': 'BTC', 'block_trade_tick_size': 0.01, 'tick_size': 0.5, 'contract_size': 10.0, 'is_active': True, 'expiration_timestamp': 32503708800000, 'instrument_type': 'reversed', 'taker_commission': 0.0005, 'maker_commission': 0.0, 'instrument_name': 'BTC-PERPETUAL', 'kind': 'future', 'rfq': False, 'price_index': 'btc_usd'}, {'tick_size_steps': [], 'quote_currency': 'USD', 'min_trade_amount': 1, 'counter_currency': 'USD', 'settlement_currency': 'ETH', 'block_trade_min_trade_amount': 100000, 'block_trade_commission': 0.00025, 'max_liquidation_commission': 0.009, 'max_leverage': 25, 'future_type': 'reversed', 'creation_timestamp': 1726819212000, 'settlement_period': 'week', 'instrument_id': 370612, 'base_currency': 'ETH', 'block_trade_tick_size': 0.01, 'tick_size': 0.25, 'contract_size': 1, 'is_active': True, 'expiration_timestamp': 1728028800000, 'instrument_type': 'reversed', 'taker_commission': 0.0005, 'maker_commission': -0.0001, 'instrument_name': 'ETH-4OCT24', 'kind': 'future', 'rfq': False, 'price_index': 'eth_usd'}, {'tick_size_steps': [], 'quote_currency': 'USD', 'min_trade_amount': 1, 'counter_currency': 'USD', 'settlement_currency': 'ETH', 'block_trade_min_trade_amount': 100000, 'block_trade_commission': 0.00025, 'max_liquidation_commission': 0.009, 'max_leverage': 25, 'future_type': 'reversed', 'creation_timestamp': 1727424017000, 'settlement_period': 'week', 'instrument_id': 373086, 'base_currency': 'ETH', 'block_trade_tick_size': 0.01, 'tick_size': 0.25, 'contract_size': 1, 'is_active': True, 'expiration_timestamp': 1728633600000, 'instrument_type': 'reversed', 'taker_commission': 0.0005, 'maker_commission': -0.0001, 'instrument_name': 'ETH-11OCT24', 'kind': 'future', 'rfq': False, 'price_index': 'eth_usd'}, {'tick_size_steps': [], 'quote_currency': 'USD', 'min_trade_amount': 1, 'counter_currency': 'USD', 'settlement_currency': 'ETH', 'block_trade_min_trade_amount': 100000, 'block_trade_commission': 0.00025, 'max_liquidation_commission': 0.009, 'max_leverage': 50, 'future_type': 'reversed', 'creation_timestamp': 1552568454000, 'settlement_period': 'perpetual', 'instrument_id': 210760, 'base_currency': 'ETH', 'block_trade_tick_size': 0.01, 'tick_size': 0.05, 'contract_size': 1, 'is_active': True, 'expiration_timestamp': 32503708800000, 'instrument_type': 'reversed', 'taker_commission': 0.0005, 'maker_commission': 0.0, 'instrument_name': 'ETH-PERPETUAL', 'kind': 'future', 'rfq': False, 'price_index': 'eth_usd'}]
 
-def extract_currency_from_text(words: str) -> str:
-    """
-    Extracting currency from channel message
-    
-     some variables:
-     chart.trades.BTC-PERPETUAL.1
-     incremental_ticker.BTC-4OCT24
-    """
-    if "."in words:
-          filter1= (words.partition('.')[2]).lower()
-          print(filter1)
-          
-          if "."in filter1:
-            filter1= (filter1.partition('.')[2]).lower()
-            print(filter1)
-      
-    else:
-          filter1= (words.partition('.')[0]).lower()
-        
-    return (filter1.partition('-')[0]).lower()
-
-print(extract_currency_from_text("chart.trades.BTC-PERPETUAL.1"))
-        
+print([','.join(str(o)) for o in data])
