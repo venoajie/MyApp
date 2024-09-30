8a354c908126511b64d2a27007175ba5975b487e
venoajie <59521823+venoajie@users.noreply.github.com>
2024-09-30 21:12:35 +0700

    .

--- src/uji_hapus copy.py
+++ src/uji_hapus copy.py
@@ -26,7 +26,6 @@ def get_config(file_name: str) -> list:
         return []
 
 
-file_toml = "config_strategies.toml"
-table = "strategies"
+file_toml =  [{'strategy_label': 'hedgingSpot', 'is_active': True, 'contribute_to_hedging': True, 'cancellable': True, 'side': 'sell', 'take_profit_pct': 0, 'settlement_period': ['perpetual'], 'weighted_factor': {'minimum': 1, 'medium': 5, 'extreme': 10, 'flash_crash': 20}, 'waiting_minute_before_cancel': 3, 'halt_minute_before_reorder': 240, 'max_leverage': 1, 'delta_price_pct': 0.005}]
 
-print (get_config(file_toml))
+print ((file_toml[0]))
