e5973b38af49288c5a281de506dfbb8323732882
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-03 02:24:46 +0700

    .

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -1,13 +1,4 @@
 
-from websocket_management.ws_management import (get_config)
-
-
-file_toml = "config_strategies.toml"
-    
-config_app = get_config(file_toml)
-
-strategies_config_app = config_app["strategies"]
-
-weighted_factor= [o["weighted_factor"] for o in strategies_config_app if "hedgingSpot" in o["strategy_label"]]
-print (f"weighted_factor {weighted_factor}")
-waiting_minute_before_cancel= [o["waiting_minute_before_cancel"] for o in strategies_config_app if "hedgingSpot" in o["strategy_label"]]
+data_from_db_trade_open= [{'trade_id': '319556178', 'label': 'hedgingSpot-closed-1726878643143'}, {'trade_id': '319556178', 'label': 'hedgingSpot-closed-1726878643143'}, {'trade_id': '319556178', 'label': 'hedgingSpot-closed-1726878643143'}, {'trade_id': '319556178', 'label': 'hedgingSpot-closed-1726878643143'}, {'trade_id': '319556178', 'label': 'hedgingSpot-closed-1726878643143'}, {'trade_id': '319556178', 'label': 'hedgingSpot-closed-1726878643143'}]
+result_order_id= [o["trade_id"] for o in data_from_db_trade_open]
+print (f"result_order_id {result_order_id}")
