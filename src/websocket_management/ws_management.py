29688fd31733575dacb5faa7e825ba1106875294
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-10 19:35:02 +0700

    Update ws_management.py

--- src/websocket_management/ws_management.py
+++ src/websocket_management/ws_management.py
@@ -534,8 +534,6 @@ async def on_restart(currencies_default: str,
     await cancel_all()
     
     # refresh databases
-    log.warning("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-
     await get_and_save_currencies()                
     log.warning("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
     
@@ -549,10 +547,10 @@ async def on_restart(currencies_default: str,
                                        transaction_log_trading,
                                        archive_db_table)
         
-        #await update_trades_from_exchange (currency,
-        #                                   archive_db_table,
-        #                                   order_table,
-        #                                   100)
+        await update_trades_from_exchange (currency,
+                                           archive_db_table,
+                                           order_table,
+                                           100)
         #await check_db_consistencies_and_clean_up_imbalances(currency)                           
 
     log.warning("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
