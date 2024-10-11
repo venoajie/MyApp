d0686694e246aca54edd76b290937dd56940961d
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-11 15:02:31 +0700

    Update sync_with_remote.sh

--- src/sync_with_remote.sh
+++ src/sync_with_remote.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # https://lovethepenguin.com/linux-check-if-a-file-or-directory-exists-e00cfa672249
 
-flnm = basename $(find $pwd -name "*.bak")
-echo "$flnm"
+flnm = $(basename $(find $pwd -name "*.bak"))
+echo " $flnm"
 
 while true; do
 
