4eaac4d0a11017b0865fb354f29664e7cd381464
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-11 14:59:26 +0700

    Update sync_with_remote.sh

--- src/sync_with_remote.sh
+++ src/sync_with_remote.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # https://lovethepenguin.com/linux-check-if-a-file-or-directory-exists-e00cfa672249
 
+flnm = basename $(find $pwd -name "*.bak")
+echo "${flnm}"
+
 while true; do
 
-    if test -f databases/*.bak;
+    if test -f databases/"${flnm}";
         then
                 echo "file exists"
                 echo "Moving local  files to remote..."
@@ -11,23 +14,26 @@ while true; do
                 
                 sleep 5s
 
-                rclone sync --include databases/*.bak remote:/remote-sqlite
+                #rclone sync --include databases/*.bak remote:/remote-sqlite
                 echo "changes directory"
-                cd databases 
-                rm *.bak
-                cd ..
+                #cd databases 
+                #rm *.bak
+                #cd ..
                 
                 echo "sync_with_remote: sleep 15 minutes"
-                sleep 15m
+                #sleep 15m
 
         else
                 echo "sync_with_remote: file does not exist"
-                rclone sync --include databases/*.bak remote:/remote-sqlite
+                #rclone sync --include databases/*.bak remote:/remote-sqlite
+
+                #clone sync databases  b2:/remote-sqlite  --include *.{bak}
+
                 echo "changes directory"
                 sleep 5s
-                cd databases 
-                rm *.bak
-                cd ..
+                #cd databases 
+                #rm *.bak
+                c#d ..
                 
                 #rclone sync  databases/exchanges/deribit/transactions/eth-myTrades-open-recovery-point.pkl b2:MyAppTrading
                 
