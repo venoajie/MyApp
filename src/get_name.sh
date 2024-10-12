48cc702aeeab02fff1240ff99ca00242580676bb
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-12 10:13:52 +0700

    .

--- src/get_name.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in $(find $pwd -name "*.bak")
-do 
-  flnm=$(basename $i)
-  extnm="${flnm##*.}"
-  flnmnoext="${flnm%.*}"
-  echo "FileName is : " $flnm " Extension is : " $extnm " File Name without extension is $flnmnoext"
-done
