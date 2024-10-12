881f741244b17afe91bb574efdcffab08cd0dac2
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-12 10:02:05 +0700

    Update sync_with_remote.sh

--- src/sync_with_remote.sh
+++ src/sync_with_remote.sh
@@ -3,12 +3,10 @@
 
 while true; do
 
-
     if test -f databases/"$(basename $(find $pwd -name "*.bak"))";
         then
                 echo "file exists"
                 echo "Moving local  files to remote..."
-                #rclone sync  databases/exchanges/deribit/transactions/eth-myTrades-open-recovery-point.pkl oci:bucket-20230107-0704
                 
                 rclone sync databases/"$(basename $(find $pwd -name "*.bak"))" remote:/remote-sqlite
 
@@ -22,13 +20,10 @@ while true; do
 
         else
                 echo "sync_with_remote: file does not exist yet"
-                #rclone sync --include databases/*.bak remote:/remote-sqlite
 
-                #clone sync databases  b2:/remote-sqlite  --include *.{bak}
                 echo "wait file for exist: sleep 10 minutes"
                 sleep 10m
                 
-                #rclone sync  databases/exchanges/deribit/transactions/eth-myTrades-open-recovery-point.pkl b2:MyAppTrading
                 
     fi
 done
