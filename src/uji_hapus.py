0534853fdf18a48c88f38045522e44d3de65953a
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-17 10:32:58 +0700

    Update uji_hapus.py

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -1,3 +1,8 @@
-a= ["A", "B", "D"]
-x= ["A", "B", "C", "D"]
-print([o for o in x if o in a])
+from cachetools import TTLCache
+cache = TTLCache(maxsize=1, ttl=100)
+cache['dog'] = 'dog'
+a = cache['dog'] # dog
+cache['dog']
+
+#print (cache['dog'])    
+#print (cache)
